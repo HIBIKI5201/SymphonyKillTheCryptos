aa02c642ee46e44418d62cb1295bc683311f6542
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-02 00:43:43 +0900

    [add]モックのタイピングを制作

---
+++ Assets/Scripts/Develop/PlayerDev.cs
@@ -0,0 +1,33 @@
+using Cryptos.Runtime.System;
+using SymphonyFrameWork.Attribute;
+using SymphonyFrameWork.System;
+using TMPro;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Ingame
+{
+    public class PlayerDev : MonoBehaviour
+    {
+        private TextMeshProUGUI _text;
+        
+        private void Start()
+        {
+            _text = FindAnyObjectByType<TextMeshProUGUI>();
+
+            var buffer = ServiceLocator.GetInstance<InputBuffer>();
+            buffer.OnKeyPressed += OnPressOnKey;
+        }
+
+        private void OnPressOnKey(char key)
+        {
+            string text = _text.text;
+
+            if (char.ToUpper(text[0]) == key)
+            {
+                text = text.Substring(1);
+            }
+            
+            _text.text = text;
+        }
+    }
+}
