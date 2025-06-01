80130b0f6b88bc4615ce4ae243a7dc1045c83ced
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-02 07:19:10 +0900

    [add]Symphonyのアニメマネージャーを追加

---
+++ Assets/Scripts/Runtime/Ingame/Character/SymphonyAnimeManager.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+namespace Cryptos.Runtime
+{
+    [RequireComponent(typeof(Animator))]
+    public class SymphonyAnimeManager : MonoBehaviour
+    {
+        private readonly int _dirXHash = Animator.StringToHash("DirX");
+        private readonly int _dirYHash = Animator.StringToHash("DirY");
+        private readonly int _velocityHash = Animator.StringToHash("Velocity");
+        private readonly int _sprintHash = Animator.StringToHash("IsSprint");
+
+        private Animator _animator;
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+        
+        public void SetDirX(float value) => _animator.SetFloat(_dirXHash, value);
+        public void SetDirY(float value) => _animator.SetFloat(_dirYHash, value);
+        public void SetVelocity(float value) => _animator.SetFloat(_velocityHash, value);
+        public void SetSprint(bool value) => _animator.SetBool(_sprintHash, value);
+    }
+}
