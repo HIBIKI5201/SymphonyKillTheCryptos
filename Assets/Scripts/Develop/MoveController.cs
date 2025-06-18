04bc48b5c5fda68cc56d4e607a9265e61a12040d
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-18 15:21:59 +0900

    [update]バリデーション追加

--- Assets/Scripts/Develop/MoveController.cs
+++ Assets/Scripts/Develop/MoveController.cs
@@ -3,22 +3,23 @@ using UnityEngine;
 
 namespace Cryptos.Runtime
 {
+    [RequireComponent(typeof(SymphonyAnimeManager))]
     public class MoveController : MonoBehaviour
     {
         [SerializeField, Range(0, 10)] private float _larpSpeed = 5f;
         
-        private Animator _animator;
         private SymphonyAnimeManager _symphonyAnimeManager;
         
         private Vector2 _lastDir;
         private void Start()
         {
-            _animator = GetComponent<Animator>();
             _symphonyAnimeManager = GetComponent<SymphonyAnimeManager>();
         }
 
         void Update()
         {
+            if (_symphonyAnimeManager == null) return;
+
             Vector2 dir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             
             _lastDir = Vector2.Lerp(_lastDir, dir, Time.deltaTime * _larpSpeed);
