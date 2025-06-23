ab72ee40254aee7974cb3a96c7dd110940f6c8f1
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-24 06:19:37 +0900

    [update]カードの情報を実装

--- Assets/Scripts/Runtime/Ingame/Entity/Card/CardData.cs
+++ Assets/Scripts/Runtime/Ingame/Entity/Card/CardData.cs
@@ -1,7 +1,31 @@
+using Cryptos.Runtime.System;
 using UnityEngine;
 
-
-public class CardData : ScriptableObject
+namespace Cryptos.Runtime.Ingame.Entity
 {
+    /// <summary>
+    ///     カードのデータ情報
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(CardData),
+        menuName = CryptosPathConstant.ASSET_PATH + nameof(CardData))]
+    public class CardData : ScriptableObject
+    {
+        public string CardName => _cardName;
+        public string CardExplanation => _cardExplanation;
+        public int CardDifficulty => _cardDifficulty;
+        public Texture2D CardIcon => _cardIcon;
 
+        [Header("基本情報")]
+        [SerializeField, Tooltip("カードの名前")]
+        private string _cardName;
+
+        [SerializeField, Tooltip("カードの説明"), TextArea]
+        private string _cardExplanation;
+
+        [SerializeField, Tooltip("カードの難易度")]
+        private int _cardDifficulty;
+
+        [SerializeField, Tooltip("アイコン画像")]
+        private Texture2D _cardIcon;
+    }
 }
