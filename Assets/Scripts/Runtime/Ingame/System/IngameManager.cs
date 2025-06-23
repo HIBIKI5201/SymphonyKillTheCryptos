e4497eebc9657384a1c18a7372f8e0874b108a30
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-23 19:06:51 +0900

    [add]インゲームマネージャーを追加

---
+++ Assets/Scripts/Runtime/Ingame/System/IngameManager.cs
@@ -0,0 +1,22 @@
+using BeatKeeper.Runtime.System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Ingame.System
+{
+    /// <summary>
+    ///     インゲームシーンのマネジメントを行う
+    /// </summary>
+    public class IngameManager : MonoBehaviour, IInitializeAsync
+    {
+        private readonly string[] scenes = { "Stage" };
+
+        Task IInitializeAsync.InitializeTask { get; set; }
+
+        async Task IInitializeAsync.InitializeAsync()
+        {
+            Debug.Log("ingame manager");
+            await MultiSceneLoader.LoadScene(scenes);
+        }
+    }
+}
