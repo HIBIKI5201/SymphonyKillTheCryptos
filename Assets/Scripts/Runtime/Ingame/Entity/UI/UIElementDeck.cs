98de8d7a63cb651b519cd1eae9cb9672b16ea76e
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-24 05:44:50 +0900

    [add]デッキのCSを追加

---
+++ Assets/Scripts/Runtime/Ingame/Entity/UI/UIElementDeck.cs
@@ -0,0 +1,17 @@
+using SymphonyFrameWork.Utility;
+using System.Threading.Tasks;
+using UnityEngine.UIElements;
+
+namespace Cryptos.Runtime.Ingame.UI
+{
+    [UxmlElement]
+    public partial class UIElementDeck : SymphonyVisualElement
+    {
+        public UIElementDeck() : base("Assets/Resources/UIToolKit/UXML/Ingame/Deck") { }
+
+        protected override Task Initialize_S(TemplateContainer container)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
