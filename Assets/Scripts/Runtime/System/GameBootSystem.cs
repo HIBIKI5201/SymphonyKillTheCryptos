5feef6d15224b5e21bb852dc96d0c829fcbd5d54
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-23 19:06:37 +0900

    [add]ブートシステムを追加

---
+++ Assets/Scripts/Runtime/System/GameBootSystem.cs
@@ -0,0 +1,28 @@
+using BeatKeeper.Runtime.System;
+using SymphonyFrameWork.System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Cryptos.Runtime.System
+{
+    public class GameBootSystem : MonoBehaviour
+    {
+        [SerializeField, Tooltip("初期シーン")]
+        private SceneListEnum firstScene;
+
+        private async void Awake()
+        {
+            //現在のシーンをアンロードする
+            Scene activeScene = SceneManager.GetActiveScene();
+            await SceneLoader.UnloadScene(activeScene.name);
+
+            string sceneName = firstScene.ToString();
+
+            //初期シーンをロードする
+            await MultiSceneLoader.LoadScene(sceneName);
+            SceneLoader.SetActiveScene(sceneName);
+
+            Destroy(gameObject); //終了したら自己破棄する
+        }
+    }
+}
