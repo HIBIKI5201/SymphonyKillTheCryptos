c37ee10b931b6857231cfae87947cbef22ddeafc
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-23 22:29:21 +0900

    [update]フレームワークに初期化処理を組み込み

--- Assets/Scripts/Runtime/System/MultiSceneLoader.cs
+++ Assets/Scripts/Runtime/System/MultiSceneLoader.cs
@@ -1,10 +1,7 @@
 using SymphonyFrameWork.System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 
-namespace BeatKeeper.Runtime.System
+namespace Cryptos.Runtime.System
 {
     /// <summary>
     ///     シーンをロードする処理を担当する
@@ -26,41 +23,6 @@ namespace BeatKeeper.Runtime.System
             }
 
             await Task.WhenAll(tasks); //全てのロード終了まで待機
-
-            tasks = new Task[names.Length];
-            for (int i = 0; i < names.Length; i++)
-            {
-                //各シーンのInitializeを
-                if (SceneLoader.GetExistScene(names[i], out var scene))
-                {
-                    tasks[i] = SceneInitialize(scene);
-                }
-            }
-
-            await Task.WhenAll(tasks); //全ての初期化が終了するまで待機
-        }
-
-        private static async Task SceneInitialize(Scene scene)
-        {
-            GameObject[] objs = scene.GetRootGameObjects();
-
-            if (objs.Length <= 0) return;
-
-            List<Task> tasks = new();
-
-            //オブジェクト達の初期化を登録
-            for (int i = 0; i < objs.Length; i++)
-            {
-                if (objs[i].TryGetComponent<IInitializeAsync>
-                    (out var initialize))
-                {
-                    tasks.Add(initialize.DoInitialize());
-                }
-            }
-
-            if (tasks.Count <= 0) return; //タスクが無ければ終了
-
-            await Task.WhenAll(tasks);
         }
     }
 }
