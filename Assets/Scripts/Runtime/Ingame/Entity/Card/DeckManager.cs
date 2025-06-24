33cc21b0655c201786852de42902f9fbda1a96a9
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-24 17:21:22 +0900

    [update]UIにデッキを表示させる

--- Assets/Scripts/Runtime/Ingame/Entity/Card/DeckManager.cs
+++ Assets/Scripts/Runtime/Ingame/Entity/Card/DeckManager.cs
@@ -1,28 +1,63 @@
+using Cryptos.Runtime.Ingame.UI;
+using SymphonyFrameWork;
+using SymphonyFrameWork.System;
+using SymphonyFrameWork.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Cryptos.Runtime.Ingame.Entity
 {
-    public class DeckManager : MonoBehaviour
+    public class DeckManager : MonoBehaviour, IInitializeAsync
     {
         [SerializeField, Tooltip("ワードのデータベース")]
-        private WordDataBase _wordData;
+        private WordDataBase _wordDatabase;
 
-        private List<CardInstance> _deckCard = new();
+        Task IInitializeAsync.InitializeTask { get;  set; }
+        private IngameUIManager _ingameUIManager;
 
+        private readonly List<CardInstance> _deckCard = new();
+
+        [SerializeField, Obsolete]
+        private CardData _cardData;
+
+        async Task IInitializeAsync.InitializeAsync()
+        {
+            _ingameUIManager = await ServiceLocator.GetInstanceAsync<IngameUIManager>();
+        }
+
+        private async void Start()
+        {
+            await SymphonyTask.WaitUntil(() => (this as IInitializeAsync).IsDone);
+            AddCardToDeck(_cardData);
+        }
+
+        /// <summary>
+        ///     カードを追加する
+        /// </summary>
+        /// <param name="data"></param>
         public void AddCardToDeck(CardData data)
         {
             List<WordData> words = new ();
 
             //カードの難易度までのワードを追加
-            for (int i = 0; i < data.CardDifficulty; i++) 
+            for (int i = 0; i <= data.CardDifficulty; i++) 
             {
-                words.Concat(_wordData.WordData[i]);
+                words.AddRange(_wordDatabase[i]);
+            }
+
+            if (words.Count <= 0)
+            {
+                Debug.LogError("ワードが一つもありません");
+                return;
             }
 
             CardInstance instance = new(data, words.ToArray());
             _deckCard.Add(instance);
+
+            _ingameUIManager?.UIElementDeck.AddCard(instance);
         }
     }
 }
