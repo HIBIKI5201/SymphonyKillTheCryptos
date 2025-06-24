42a2533cee6bf98154d3cd4efb4b00f65ce2b610
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-24 19:19:21 +0900

    [update]文字列が被らなくする

--- Assets/Scripts/Runtime/Ingame/Entity/Card/WordManager.cs
+++ Assets/Scripts/Runtime/Ingame/Entity/Card/WordManager.cs
@@ -13,16 +13,18 @@ namespace Cryptos.Runtime.Ingame.Entity
         /// </summary>
         /// <param name="candidateWords">候補となるワード</param>
         /// <returns></returns>
-        public WordData GetOnlyWord(WordData[] candidateWords)
+        public WordData GetAvailableWord(IEnumerable<WordData> candidateWords)
         {
             //既にデッキ上に含まれているものを除外
-            IEnumerable<WordData> onlyWords = candidateWords
-                .Where(word => !_existWords.Contains(word));
 
-            if (onlyWords.Any()) //残っていたらランダムなワードを返す
+            candidateWords = GetOnlyWords(candidateWords);
+            candidateWords = GetNonStartWithWords(candidateWords); 
+
+
+            if (candidateWords.Any()) //残っていたらランダムなワードを返す
             {
-                int random = Random.Range(0, onlyWords.Count());
-                return onlyWords.ElementAt(random);
+                int random = Random.Range(0, candidateWords.Count());
+                return candidateWords.ElementAt(random);
             }
             else
             {
@@ -33,5 +35,41 @@ namespace Cryptos.Runtime.Ingame.Entity
 
         public void AddWord(WordData word) => _existWords.Add(word);
         public void RemoveWord(WordData word) => _existWords.Remove(word);
+
+        private IEnumerable<WordData> GetOnlyWords(IEnumerable<WordData> wordDatas)
+        {
+            return wordDatas.Where(word => !_existWords.Contains(word));
+        }
+
+        /// <summary>
+        ///     文字列の最初が被っていないワードのみに厳選する
+        /// </summary>
+        /// <param name="wordDatas"></param>
+        /// <returns></returns>
+        private IEnumerable<WordData> GetNonStartWithWords(IEnumerable<WordData> wordDatas)
+        {
+            List<WordData> result = new();
+
+            foreach (var candidate in wordDatas)
+            {
+                bool isConflicting = false;
+
+                foreach (var exist in _existWords) //全てのワードと被らないか確認
+                {
+                    if (candidate.Word.StartsWith(exist.Word) || exist.Word.StartsWith(candidate.Word))
+                    {
+                        isConflicting = true;
+                        break;
+                    }
+                }
+
+                if (!isConflicting) //もし一つも被らなければ候補に追加
+                {
+                    result.Add(candidate);
+                }
+            }
+
+            return result;
+        }
     }
 }
