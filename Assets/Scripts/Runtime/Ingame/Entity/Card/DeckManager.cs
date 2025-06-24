1aa860c80d8aeabd847c117750d29e4777cdc869
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-24 16:35:58 +0900

    [update]難易度までのワードを追加する

--- Assets/Scripts/Runtime/Ingame/Entity/Card/DeckManager.cs
+++ Assets/Scripts/Runtime/Ingame/Entity/Card/DeckManager.cs
@@ -1,9 +1,28 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Cryptos.Runtime.Ingame.Entity
 {
     public class DeckManager : MonoBehaviour
     {
+        [SerializeField, Tooltip("ワードのデータベース")]
+        private WordDataBase _wordData;
+
+        private List<CardInstance> _deckCard = new();
+
+        public void AddCardToDeck(CardData data)
+        {
+            List<WordData> words = new ();
+
+            //カードの難易度までのワードを追加
+            for (int i = 0; i < data.CardDifficulty; i++) 
+            {
+                words.Concat(_wordData.WordData[i]);
+            }
+
+            CardInstance instance = new(data, words.ToArray());
+            _deckCard.Add(instance);
+        }
     }
 }
