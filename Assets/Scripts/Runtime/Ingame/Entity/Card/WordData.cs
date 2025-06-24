811c94d74f4112d3bd73a80f482862fe9febff63
HIBIKI_5201 <takuto5201@gmail.com>
2025-06-25 03:54:25 +0900

    [update]ワードデータをワードの配列に変更

--- Assets/Scripts/Runtime/Ingame/Entity/Card/WordData.cs
+++ Assets/Scripts/Runtime/Ingame/Entity/Card/WordData.cs
@@ -6,11 +6,11 @@ namespace Cryptos.Runtime.Ingame.Entity
     [Serializable]
     public class WordData
     {
-        public string Word => _word;
+        public string[] Words => _words;
         public int Difficulty => _difficulty;
 
         [SerializeField, Tooltip("文字列")]
-        private string _word;
+        private string[] _words;
 
         [SerializeField, Tooltip("難易度"), Min(1)]
         private int _difficulty;
