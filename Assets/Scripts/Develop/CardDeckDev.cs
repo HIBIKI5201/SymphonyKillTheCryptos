161cb0bc2855bd41b91f4b7f3686161bf6a1f2d5
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-08 14:06:55 +0900

    [add]カードのテストコードを追加

---
+++ Assets/Scripts/Develop/CardDeckDev.cs
@@ -0,0 +1,47 @@
+using Cryptos.Runtime.Entity.Ingame.Card;
+using SymphonyFrameWork;
+using SymphonyFrameWork.System;
+using SymphonyFrameWork.Utility;
+using System;
+using UnityEngine;
+
+namespace Cryptos.Runtime
+{
+    public class CardDeckDev : MonoBehaviour
+    {
+        [Header("テストコード")]
+        [SerializeField]
+        private CardData[] _cardDatas;
+        [SerializeField, Min(1)]
+        private int _cardAmount = 3;
+
+        private async void Start()
+        {
+            DeckManager deckManager = await ServiceLocator.GetInstanceAsync<DeckManager>();
+            await SymphonyTask.WaitUntil(() => (deckManager as IInitializeAsync).IsDone);
+
+            TestCardSpawn(deckManager);
+        }
+
+        private void TestCardSpawn(DeckManager deckManager)
+        {
+            if (_cardDatas == null || _cardDatas.Length == 0)
+            {
+                Debug.LogWarning("カードデータが設定されていません。");
+                return;
+            }
+
+            for (int i = 0; i < _cardAmount; i++)
+                RandomDraw();
+
+            void RandomDraw(CardInstance ins = default)
+            {
+                Debug.Log("draw");
+                var cardData = _cardDatas[UnityEngine.Random.Range(0, _cardDatas.Length)];
+                var instance = deckManager.AddCardToDeck(cardData);
+
+                instance.OnComplete += RandomDraw;
+            }
+        }
+    }
+}
