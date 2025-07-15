9720225d9cc4fcfa546dedd8d59dd2ed3e244dc7
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-16 00:40:06 +0900

    [add]攻撃可能なキャラのインターフェースを追加

---
+++ Assets/Scripts/Runtime/Entity/Ingame/Character/IAttackable.cs
@@ -0,0 +1,12 @@
+using SymphonyFrameWork;
+
+namespace Cryptos.Runtime.Entity
+{
+    /// <summary>
+    ///     攻撃が可能なオブジェクトのインターフェース
+    /// </summary>
+    public interface IAttackable : IGameObject
+    {
+
+    }
+}
