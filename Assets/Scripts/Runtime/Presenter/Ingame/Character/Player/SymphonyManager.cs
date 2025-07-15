bc5174c5165a6eeb834f8cd466e250ec39bc7af2
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-16 00:53:48 +0900

    [update]SymphonyManagerにデータの定義を追加

--- Assets/Scripts/Runtime/Presenter/Ingame/Character/Player/SymphonyManager.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Player/SymphonyManager.cs
@@ -8,6 +8,13 @@ namespace Cryptos.Runtime.Presenter.Character.Player
     /// </summary>
     public class SymphonyManager : MonoBehaviour, IAttackable, IHitable
     {
+        public IHitableData HitableData => _hitableData;
+        public IAttackableData AttackableData => _attackableData;
+
+        private IHitableData _hitableData;
+        private IAttackableData _attackableData;
+
+
         private SymphonyAnimeManager _animeManager;
 
         private void Awake()
