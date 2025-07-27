79fad30a79663e52913e5f4f81233f6df515429d
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-28 03:06:28 +0900

    [update]敵を生成するメソッドを追加

--- Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyManager.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyManager.cs
@@ -1,4 +1,4 @@
-using Cryptos.Runtime.Entity;
+using Cryptos.Runtime.Entity.Ingame.Character;
 using Cryptos.Runtime.UseCase.Ingame.Character;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,9 +14,23 @@ namespace Cryptos.Runtime.Presenter.Ingame.Character
             return _enemies;
         }
 
-        private void Awake()
+        public CharacterEntity<EnemyData> CreateEnemy(EnemyData data)
         {
-            
+            if (data == null)
+            {
+                Debug.LogError("EnemyData is null.");
+                return null;
+            }
+            CharacterEntity<EnemyData> enemy = _enemyGenerator.Generate(data);
+            if (enemy != null)
+            {
+                _enemies.Add(enemy);
+                enemy.OnDead += () =>
+                {
+                    _enemies.Remove(enemy);
+                };
+            }
+            return enemy;
         }
 
         private EnemyGenerator _enemyGenerator = new();
