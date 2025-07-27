2b4f4819611614e918d56828a88b606af5f3f280
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-28 02:32:03 +0900

    [add]敵のデータクラスを追加

---
+++ Assets/Scripts/Runtime/Entity/Ingame/Character/EnemyData.cs
@@ -0,0 +1,31 @@
+using Cryptos.Runtime.Framework;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Entity.Ingame.Character
+{
+    [CreateAssetMenu(fileName = nameof(EnemyData), menuName = CryptosPathConstant.ASSET_PATH + nameof(EnemyData), order = 1)]
+    public class EnemyData : ScriptableObject, IAttackableData, IHitableData
+    {
+        public float AttackPower => _attackPower;
+
+        public float CriticalChance => _criticalChance;
+        public float CriticalDamage => _criticalDamage;
+
+        public float MaxHealth => _maxHealth;
+
+        public float Armor => _armor;
+
+        [SerializeField]
+        private float _attackPower;
+        [SerializeField]
+        private float _criticalChance;
+        [SerializeField]
+        private float _criticalDamage;
+        [SerializeField]
+        private float _health;
+        [SerializeField]
+        private float _maxHealth;
+        [SerializeField]
+        private float _armor;
+    }
+}
