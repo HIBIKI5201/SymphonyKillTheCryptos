ecec6a74e349831b718cec78867a08e1bb9a7188
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-28 02:34:14 +0900

    [update]敵のジェネレーターはキャラクターエンティティを生成するように

--- Assets/Scripts/Runtime/UseCase/Ingame/Character/EnemyGenerator.cs
+++ Assets/Scripts/Runtime/UseCase/Ingame/Character/EnemyGenerator.cs
@@ -1,9 +1,19 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
 using UnityEngine;
 
 namespace Cryptos.Runtime.UseCase.Ingame.Character
 {
     public class EnemyGenerator
     {
-
+        public CharacterEntity<EnemyData> Generate(EnemyData data)
+        {
+            if (data == null)
+            {
+                Debug.LogError("EnemyData is null.");
+                return null;
+            }
+            // ここで敵キャラクターを生成する
+            return new CharacterEntity<EnemyData>(data);
+        }
     }
 }
