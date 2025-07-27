d370f16624d5acc331c17bd25a92d8d6c866b2e0
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-27 22:23:14 +0900

    [update]プレイヤーのエンティティを追加

---
+++ Assets/Scripts/Runtime/Entity/Ingame/Character/PlayerCharacterEntity.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Cryptos.Runtime.Entity.Ingame.Character
+{
+    public class PlayerCharacterEntity : CharacterEntity<SymphonyData>
+    {
+        public PlayerCharacterEntity(SymphonyData data) : base(data)
+        {
+            _symphonyData = data;
+        }
+
+        public override float GetAttackPower()
+        {
+            float power = _symphonyData.AttackPower;
+
+            //クリティカル時に倍率を掛ける
+            if (UnityEngine.Random.Range(0, 1) < _symphonyData.CriticalChance)
+            {
+                power *= _symphonyData.CriticalDamage;
+            }
+
+            return power;
+        }
+
+        private readonly SymphonyData _symphonyData;
+    }
+}
