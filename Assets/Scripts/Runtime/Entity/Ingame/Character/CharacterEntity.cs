e063735b20e2a123562f84698e63ac31a60ff166
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-28 02:33:24 +0900

    [update]エンティティクラスでデータを保持するように

--- Assets/Scripts/Runtime/Entity/Ingame/Character/CharacterEntity.cs
+++ Assets/Scripts/Runtime/Entity/Ingame/Character/CharacterEntity.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 namespace Cryptos.Runtime.Entity.Ingame.Character
 {
-    public abstract class CharacterEntity<T> : IAttackable, IHitable
+    public class CharacterEntity<T> : IAttackable, IHitable
         where T : class, IAttackableData, IHitableData
     {
         public CharacterEntity(T data)
         {
-            _healthEntity = new HealthEntity(data.MaxHealth);
+            _entityData = data;
+            _healthEntity = new HealthEntity(data);
         }
 
         [Tooltip("第一引数は現在値、第二引数は最大値")]
@@ -25,12 +26,24 @@ namespace Cryptos.Runtime.Entity.Ingame.Character
             remove => _healthEntity.OnDead -= value;
         }
 
-        public abstract float GetAttackPower();
+        public virtual float GetAttackPower()
+        {
+            float power = _entityData.AttackPower;
 
-        public void AddHealthDamage(float damage) => _healthEntity.AddHealthDamage(damage);
-        public void AddHealthHeal(float amount) => _healthEntity.AddHealthHeal(amount);
-        public void Dead() => _healthEntity.Dead();
+            //クリティカル時に倍率を掛ける
+            if (UnityEngine.Random.Range(0, 1) < _entityData.CriticalChance)
+            {
+                power *= _entityData.CriticalDamage;
+            }
 
+            return power;
+        }
+
+        public virtual void AddHealthDamage(float damage) => _healthEntity.AddHealthDamage(damage);
+        public virtual void AddHealthHeal(float amount) => _healthEntity.AddHealthHeal(amount);
+        public virtual void Dead() => _healthEntity.Dead();
+
+        protected readonly T _entityData;
         private readonly HealthEntity _healthEntity;
     }
 }
