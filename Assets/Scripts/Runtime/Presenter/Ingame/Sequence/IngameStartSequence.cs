14fb381a5adc2b2e8565bde2545a81e57651d4bf
HIBIKI_5201 <takuto5201@gmail.com>
2025-07-28 03:31:15 +0900

    [add]シークエンスを追加

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/Sequence/IngameStartSequence.cs
@@ -0,0 +1,57 @@
+using Cryptos.Runtime.Entity.Ingame.Card;
+using Cryptos.Runtime.Presenter.Character.Player;
+using Cryptos.Runtime.Presenter.Ingame.Character;
+using SymphonyFrameWork;
+using SymphonyFrameWork.System;
+using SymphonyFrameWork.Utility;
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Presenter.Ingame.Sequence
+{
+    [Serializable]
+    public class IngameStartSequence
+    {
+        [Header("テストコード")]
+        [SerializeField]
+        private CardData[] _cardDatas;
+        [SerializeField, Min(1)]
+        private int _cardAmount = 3;
+
+        public async Task StartSequence()
+        {
+            DeckManager deck = await ServiceLocator.GetInstanceAsync<DeckManager>();
+            SymphonyManager player = await ServiceLocator.GetInstanceAsync<SymphonyManager>();
+            EnemyManager enemy = await ServiceLocator.GetInstanceAsync<EnemyManager>();
+
+            if (deck is IInitializeAsync initialize)
+            {
+                await SymphonyTask.WaitUntil(() => initialize.IsDone);
+            }
+
+            TestCardSpawn(deck);
+        }
+
+        private void TestCardSpawn(DeckManager deckManager)
+        {
+            if (_cardDatas == null || _cardDatas.Length == 0)
+            {
+                Debug.LogWarning("カードデータが設定されていません。");
+                return;
+            }
+
+            for (int i = 0; i < _cardAmount; i++)
+                RandomDraw();
+
+            void RandomDraw()
+            {
+                Debug.Log("draw");
+                var cardData = _cardDatas[UnityEngine.Random.Range(0, _cardDatas.Length)];
+                var instance = deckManager.AddCardToDeck(cardData);
+
+                instance.OnComplete += RandomDraw;
+            }
+        }
+    }
+}
