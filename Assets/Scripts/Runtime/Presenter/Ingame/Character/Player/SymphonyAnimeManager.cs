afda49cefd1d16aacc9176b3a26e176a19323d11
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-03 01:48:10 +0900

    [update]コード規定に従い修正

--- Assets/Scripts/Runtime/Presenter/Ingame/Character/Player/SymphonyAnimeManager.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Player/SymphonyAnimeManager.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 
 namespace Cryptos.Runtime.Presenter.Character.Player
@@ -5,27 +6,39 @@ namespace Cryptos.Runtime.Presenter.Character.Player
     [RequireComponent(typeof(Animator))]
     public class SymphonyAnimeManager : MonoBehaviour
     {
-        private readonly int _dirXHash = Animator.StringToHash("DirX");
-        private readonly int _dirYHash = Animator.StringToHash("DirY");
-        private readonly int _velocityHash = Animator.StringToHash("Velocity");
-        private readonly int _sprintHash = Animator.StringToHash("IsSprint");
-        private readonly int _skillHash = Animator.StringToHash("Skill");
-        private readonly int _skillTriggerHash = Animator.StringToHash("SkillTrigger");
-
-        private Animator _animator;
-        private void Awake()
-        {
-            _animator = GetComponent<Animator>();
-        }
-
         public void SetDirX(float value) => _animator.SetFloat(_dirXHash, value);
         public void SetDirY(float value) => _animator.SetFloat(_dirYHash, value);
         public void SetVelocity(float value) => _animator.SetFloat(_velocityHash, value);
         public void SetSprint(bool value) => _animator.SetBool(_sprintHash, value);
-        public void ActiveSkill(int value)
+
+        public void ActiveSkill(int index)
         {
-            _animator.SetInteger(_skillHash, value);
+            _animator.SetInteger(_SkillIndexHash, index);
             _animator.SetTrigger(_skillTriggerHash);
+        }
+
+        private readonly int _dirXHash = Animator.StringToHash("DirX");
+        private readonly int _dirYHash = Animator.StringToHash("DirY");
+        private readonly int _velocityHash = Animator.StringToHash("Velocity");
+        private readonly int _sprintHash = Animator.StringToHash("IsSprint");
+        private readonly int _SkillIndexHash = Animator.StringToHash("SkillIndex");
+        private readonly int _skillTriggerHash = Animator.StringToHash("SkillTrigger");
+
+        private Animator _animator;
+        private AnimatorOverrideController _overrideController;
+
+        private void Awake()
+        {
+            if (!TryGetComponent(out _animator))
+            {
+                Debug.LogError("Animator component is not found on the GameObject.");
+                return;
+            }
+        }
+
+        private void Skill(int number)
+        {
+            Debug.Log($"Skill animation event{number} triggered");
         }
     }
 }
