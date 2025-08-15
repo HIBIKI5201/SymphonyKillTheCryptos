d36e67ac605117e0ec618e42bccb97f74b97fbd8
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-16 04:05:19 +0900

    [fix]誤字を修正

--- Assets/Scripts/Runtime/UseCase/Ingame/CombatSystem/CombatProcessor.cs
+++ Assets/Scripts/Runtime/UseCase/Ingame/CombatSystem/CombatProcessor.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 
 namespace Cryptos.Runtime.UseCase.Ingame.CombatSystem
 {
-    internal class CombatProssesor
+    internal class CombatProcessor
     {
         private List<ICombatHandler> _combatHandlers = new();
 
-        public CombatProssesor AddTo(ICombatHandler handler)
+        public CombatProcessor AddTo(ICombatHandler handler)
         {
             _combatHandlers.Add(handler);
             return this;
         }
 
-        public CombatContext Excute(IAttackable attacker, IHitable target)
+        public CombatContext Excute(IAttackable attacker, IHittable target)
         {
             CombatContext context = new CombatContext(attacker.AttackableData, target.HitableData, attacker.AttackableData.AttackPower);
 
