f3675566605e2b4d966ef20ba7124c5a129eaabd
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-16 08:15:50 +0900

    [update]メソッドチェーン方式から、params引数に変更

--- Assets/Scripts/Runtime/UseCase/Ingame/CombatSystem/CombatProcessor.cs
+++ Assets/Scripts/Runtime/UseCase/Ingame/CombatSystem/CombatProcessor.cs
@@ -10,34 +10,21 @@ namespace Cryptos.Runtime.UseCase.Ingame.CombatSystem
     internal class CombatProcessor
     {
         /// <summary>
-        /// 計算処理を行うハンドラーをリストに追加します。
-        /// </summary>
-        /// <param name="handler">追加する戦闘ハンドラー。</param>
-        /// <returns>メソッドチェーンのために自身のインスタンスを返します。</returns>
-        public CombatProcessor AddTo(ICombatHandler handler)
-        {
-            _combatHandlers.Add(handler);
-            return this;
-        }
-
-        /// <summary>
         /// 登録された全ての戦闘ハンドラーを実行し、最終的な戦闘結果を計算します。
         /// </summary>
         /// <param name="attacker">攻撃側のキャラクター。</param>
         /// <param name="target">防御側のキャラクター。</param>
         /// <returns>全ての計算が完了した後の最終的なCombatContext。</returns>
-        public CombatContext Execute(IAttackable attacker, IHittable target)
+        public static CombatContext Execute(IAttackable attacker, IHittable target, params ICombatHandler[] handlers)
         {
             CombatContext context = new CombatContext(attacker.AttackableData, target.HittableData, attacker.AttackableData.AttackPower);
 
-            foreach(var handler in _combatHandlers)
+            foreach(var handler in handlers)
             {
                 context = handler.Execute(context);
             }
 
             return context;
         }
-
-        private readonly List<ICombatHandler> _combatHandlers = new();
     }
 }
