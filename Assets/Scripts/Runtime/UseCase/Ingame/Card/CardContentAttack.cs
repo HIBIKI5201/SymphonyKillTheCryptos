b72b86d13719d8ea7c951329c0cb87ffcac736db
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-16 03:37:30 +0900

    [update]カードコンテキストをUseCaseに移動

---
+++ Assets/Scripts/Runtime/UseCase/Ingame/Card/CardContentAttack.cs
@@ -0,0 +1,38 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using Cryptos.Runtime.UseCase.Ingame.CombatSystem;
+using System.Text;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Entity.Ingame.Card
+{
+    public class CardContentAttack : ICardContent
+    {
+        public CardContentAttack()
+        {
+            _combatProssesor = new CombatProssesor();
+        }
+
+        public void Execute(ICharacter[] players, ICharacter[] targets)
+        {
+            StringBuilder sb = new($"CardContentAttack: Player <b>{players}</b>\n");
+
+            IAttackable player = players[0];
+
+            foreach (var terget in targets)
+            {
+                CombatContext context = _combatProssesor.Excute(player, terget);
+
+                terget.AddHealthDamage(context);
+
+                sb.Append($"\nTarget: {terget}");
+            }
+
+            Debug.Log(sb.ToString());
+        }
+
+        [SerializeField, Min(0)]
+        private float _damageScale = 1;
+
+        private CombatProssesor _combatProssesor;
+    }
+}
