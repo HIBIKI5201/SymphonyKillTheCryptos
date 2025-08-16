7b888cfec5ae527980ebba4bbaa288e373fc0150
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-17 08:02:01 +0900

    [update]スポーンポイントをスタックするように

--- Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
@@ -1,7 +1,10 @@
 using Cryptos.Runtime.Entity.Ingame.Character;
 using Cryptos.Runtime.Presenter.Character.Player;
 using Cryptos.Runtime.Presenter.Ingame.Character;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 namespace Cryptos.Runtime.Presenter.Character.Enemy
 {
@@ -38,12 +41,21 @@ namespace Cryptos.Runtime.Presenter.Character.Enemy
         private EnemyRepository _enemyRepository;
         private SymphonyPresenter _player;
 
+        private Stack<Transform> _spawnPointStack;
+
         private void Awake()
         {
             if (_enemyContainer == null)
             {
                 _enemyContainer = transform;
             }
+
+            if (_spawnPoints == null || _spawnPoints.Length == 0)
+            {
+                Debug.LogError("Spawn points are not assigned or empty.");
+                return;
+            }
+            _spawnPointStack = new(_spawnPoints);
         }
 
         private void HandleEnemyCreated(CharacterEntity<EnemyData> enemy)
@@ -64,17 +76,12 @@ namespace Cryptos.Runtime.Presenter.Character.Enemy
                 return;
             }
 
-            if (_spawnPoints == null || _spawnPoints.Length <= 0)
-            {
-                Debug.LogError("Spawn points are not assigned or empty.");
-                return;
-            }
-
-            int index = Random.Range(0, _spawnPoints.Length);
-            Transform spawnPoint = _spawnPoints[index];
+            if (!_spawnPointStack.TryPop(out Transform spawnPoint)) return;
 
             EnemyModelPresenter model = Instantiate(_enemyModel, spawnPoint.position, spawnPoint.rotation, _enemyContainer);
             model.Init(_player.transform);
+
+            enemy.OnDead += model.Dead;
         }
     }
 }
