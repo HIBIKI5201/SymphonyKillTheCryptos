0110ea5cc438e5d6d54c4d62c088230452cf7d27
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-17 05:07:11 +0900

    [add]敵のプレゼンターを追加

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Cryptos.Runtime.Presenter
+{
+    public class EnemyPresenter : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
