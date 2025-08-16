67528e5e0cdb2138fd100336cf4d6fe20a6170b8
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-17 05:22:21 +0900

    [update]モデル生成処理を追加

--- Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
@@ -1,19 +1,35 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using Cryptos.Runtime.Presenter.Ingame.Character;
 using UnityEngine;
 
-namespace Cryptos.Runtime.Presenter
+namespace Cryptos.Runtime.Presenter.Character.Enemy
 {
     public class EnemyPresenter : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
+        public void Init(EnemyRepository repository)
         {
-        
+            _enemyRepository = repository;
+            repository.OnEnemyCreated += HandleEnemyCreated;
+
+            foreach (var enemy in repository.AllEnemies)
+            {
+                CreateEnemyModel(enemy);
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private EnemyRepository _enemyRepository;
+
+        private void HandleEnemyCreated(CharacterEntity<EnemyData> enemy)
         {
-        
+            if (enemy == null) return;
+            // ここで敵キャラクターの表示や初期化を行う
+
+            CreateEnemyModel(enemy);
+        }
+
+        private void CreateEnemyModel(CharacterEntity<EnemyData> enemy)
+        {
+            Debug.Log($"Creating enemy model for: {enemy}");
         }
     }
 }
