8b25b8d8faffdd9e0fedf503910321bcc91ef3d3
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-17 07:20:18 +0900

    [update]初期化にターゲットを渡すように

--- Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/Enemy/EnemyPresenter.cs
@@ -1,4 +1,5 @@
 using Cryptos.Runtime.Entity.Ingame.Character;
+using Cryptos.Runtime.Presenter.Character.Player;
 using Cryptos.Runtime.Presenter.Ingame.Character;
 using UnityEngine;
 
@@ -6,10 +7,18 @@ namespace Cryptos.Runtime.Presenter.Character.Enemy
 {
     public class EnemyPresenter : MonoBehaviour
     {
-        public void Init(EnemyRepository repository)
+        public void Init(EnemyRepository repository, SymphonyPresenter player)
         {
             _enemyRepository = repository;
             repository.OnEnemyCreated += HandleEnemyCreated;
+
+            destroyCancellationToken.Register(() =>
+            {
+                if (repository == null) return;
+                repository.OnEnemyCreated -= HandleEnemyCreated;
+            });
+
+            _player = player;
 
             foreach (var enemy in repository.AllEnemies)
             {
@@ -17,7 +26,25 @@ namespace Cryptos.Runtime.Presenter.Character.Enemy
             }
         }
 
+        [SerializeField]
+        private EnemyModelPresenter _enemyModel;
+
+        [Space]
+        [SerializeField]
+        private Transform[] _spawnPoints;
+        [SerializeField]
+        private Transform _enemyContainer;
+
         private EnemyRepository _enemyRepository;
+        private SymphonyPresenter _player;
+
+        private void Awake()
+        {
+            if (_enemyContainer == null)
+            {
+                _enemyContainer = transform;
+            }
+        }
 
         private void HandleEnemyCreated(CharacterEntity<EnemyData> enemy)
         {
@@ -30,6 +57,24 @@ namespace Cryptos.Runtime.Presenter.Character.Enemy
         private void CreateEnemyModel(CharacterEntity<EnemyData> enemy)
         {
             Debug.Log($"Creating enemy model for: {enemy}");
+
+            if (_enemyModel == null)
+            {
+                Debug.LogError("EnemyModelPresenter is not assigned.");
+                return;
+            }
+
+            if (_spawnPoints == null || _spawnPoints.Length <= 0)
+            {
+                Debug.LogError("Spawn points are not assigned or empty.");
+                return;
+            }
+
+            int index = Random.Range(0, _spawnPoints.Length);
+            Transform spawnPoint = _spawnPoints[index];
+
+            EnemyModelPresenter model = Instantiate(_enemyModel, spawnPoint.position, spawnPoint.rotation, _enemyContainer);
+            model.Init(_player.transform);
         }
     }
 }
