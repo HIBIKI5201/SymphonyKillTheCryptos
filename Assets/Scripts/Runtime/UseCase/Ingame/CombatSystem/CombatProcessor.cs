251f6a3e666f6527a4bbf26993a8e041743afb9f
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-18 20:39:44 +0900

    [update]ハンドラーのデータを追加

--- Assets/Scripts/Runtime/UseCase/Ingame/CombatSystem/CombatProcessor.cs
+++ Assets/Scripts/Runtime/UseCase/Ingame/CombatSystem/CombatProcessor.cs
@@ -1,5 +1,6 @@
 using Cryptos.Runtime.Entity.Ingame.Character;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Cryptos.Runtime.UseCase.Ingame.CombatSystem
 {
@@ -19,12 +20,69 @@ namespace Cryptos.Runtime.UseCase.Ingame.CombatSystem
         {
             CombatContext context = new CombatContext(attacker.AttackableData, target.HittableData, attacker.AttackableData.AttackPower);
 
-            foreach(var handler in handlers)
+            foreach (var handler in handlers)
             {
                 context = handler.Execute(context);
             }
 
             return context;
+        }
+
+        public class HandlerData
+        {
+            public ICombatHandler[] GetAddHandlers()
+            {
+                // Add側を (キー, インスタンス, 種類) で展開
+                var addHandlers = _addHHandlers
+                    .SelectMany(kvp => kvp.Value.Select(v => new { Key = kvp.Key, Type = 0, Handler = v }));
+
+                // Multiply側を (キー, インスタンス, 種類) で展開
+                var multiplyHandlers = _multiplyHandler
+                    .SelectMany(kvp => kvp.Value.Select(v => new { Key = kvp.Key, Type = 1, Handler = v }));
+
+                // 両方を結合 → Key昇順, Type昇順(add=0が先)
+                var merged = addHandlers
+                    .Concat(multiplyHandlers)
+                    .OrderBy(x => x.Key)
+                    .ThenBy(x => x.Type)
+                    .Select(x => x.Handler)
+                    .ToArray();
+
+                return merged;
+            }
+
+            public HandlerData AddHandler(HandlerType type, int id, ICombatHandler handler)
+            {
+                if (type == HandlerType.Add)
+                {
+                    if (!_addHHandlers.TryGetValue(id, out var handlers))
+                    {
+                        handlers = new ICombatHandler[0];
+                    }
+                    var newHandlers = new List<ICombatHandler>(handlers) { handler };
+                    _addHHandlers[id] = newHandlers.ToArray();
+                }
+                else if (type == HandlerType.Multiply)
+                {
+                    if (!_multiplyHandler.TryGetValue(id, out var handlers))
+                    {
+                        handlers = new ICombatHandler[0];
+                    }
+                    var newHandlers = new List<ICombatHandler>(handlers) { handler };
+                    _multiplyHandler[id] = newHandlers.ToArray();
+                }
+
+                return this;
+            }
+
+            private SortedDictionary<int, ICombatHandler[]> _addHHandlers = new();
+            private SortedDictionary<int, ICombatHandler[]> _multiplyHandler = new();
+        }
+
+        public enum HandlerType
+        {
+            Multiply,
+            Add,
         }
     }
 }
