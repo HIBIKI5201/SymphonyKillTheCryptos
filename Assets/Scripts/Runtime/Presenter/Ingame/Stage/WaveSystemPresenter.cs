76282b43d0c1586a3e3ed64c070a137ea87c80eb
HIBIKI_5201 <takuto5201@gmail.com>
2025-08-27 11:51:32 +0900

    [update]ウェーブ変更時にSpline移動を行うように

--- Assets/Scripts/Runtime/Presenter/Ingame/Stage/WaveSystemPresenter.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/Stage/WaveSystemPresenter.cs
@@ -1,31 +1,60 @@
 using Cryptos.Runtime.Entity.Ingame.Character;
 using Cryptos.Runtime.Entity.Ingame.System;
+using Cryptos.Runtime.Presenter.Character.Player;
 using Cryptos.Runtime.Presenter.Ingame.Character;
 using Cryptos.Runtime.UseCase.Ingame.System;
+using System.Threading.Tasks;
 
 namespace Cryptos.Runtime.Presenter.Ingame.System
 {
     public class WaveSystemPresenter
     {
-        public WaveSystemPresenter(WaveEntity[] waveEntities, EnemyRepository enemyRepository)
+        public WaveSystemPresenter(WaveEntity[] waveEntities, SymphonyPresenter player,  EnemyRepository enemyRepository)
         {
             WaveUseCase waveUseCase = new(waveEntities);
-            waveUseCase.OnWaveChanged += WaveEnemysCreate;
+            waveUseCase.OnWaveChanged += HandleWaveChanged;
 
             _waveUseCase = waveUseCase;
+            _symphony = player;
             _enemyRepository = enemyRepository;
         }
 
-        public void WaveEnemysCreate(WaveEntity waveEntity)
+        public void GameStart()
+        {
+            WaveEnemysCreate(_waveUseCase.CurrentWave);
+        }
+
+        private async void HandleWaveChanged(WaveEntity newWave)
+        {
+            await _symphony.NextWave(_waveUseCase.CurrentWaveIndex);
+            WaveEnemysCreate(newWave);
+        }
+
+        private void HandleEnemyDead()
+        {
+            _enemyCount--;
+            if (_enemyCount <= 0)
+            {
+                _waveUseCase.NextWave();
+            }
+        }
+
+        private void WaveEnemysCreate(WaveEntity waveEntity)
         {
             EnemyData[] enemyData = waveEntity.Enemies;
+            _enemyCount = enemyData.Length;
+
             foreach (var item in enemyData)
             {
-                _enemyRepository.CreateEnemy(item);
+                CharacterEntity<EnemyData> enemy = _enemyRepository.CreateEnemy(item);
+                enemy.OnDead += HandleEnemyDead;
             }
         }
 
         private WaveUseCase _waveUseCase;
+        private SymphonyPresenter _symphony;
         private EnemyRepository _enemyRepository;
+
+        private int _enemyCount = 0;
     }
 }
