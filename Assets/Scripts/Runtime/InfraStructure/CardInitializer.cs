7ac82a0ac59a7758c81837796aa682949c44f9e9
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-01 01:30:50 +0900

    [add]カードの初期化処理を行う

---
+++ Assets/Scripts/Runtime/InfraStructure/CardInitializer.cs
@@ -0,0 +1,35 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using Cryptos.Runtime.Entity.Ingame.Word;
+using Cryptos.Runtime.Presenter.Ingame.Character;
+using Cryptos.Runtime.UseCase.Ingame.Card;
+using System.Linq;
+
+namespace Cryptos.Runtime.InfraStructure.Ingame.Utility
+{
+    public static class CardInitializer
+    {
+        public static CardInitializationData Initialize(
+            WordDataBase wordDataBase,
+            CharacterEntity<SymphonyData> symphony,
+            EnemyRepository enemyRepo)
+        {
+            CardUseCase cardUseCase = new(wordDataBase, new());
+            cardUseCase.GetPlayer += () => symphony;
+            cardUseCase.GetTargets += () => enemyRepo.AllEnemies.ToArray();
+
+            return new(cardUseCase);
+        }
+
+        public readonly struct CardInitializationData
+        {
+            public CardInitializationData(CardUseCase cardUseCase)
+            {
+                _cardUseCase = cardUseCase;
+            }
+
+            public CardUseCase CardUseCase => _cardUseCase;
+
+            private readonly CardUseCase _cardUseCase;
+        }
+    }
+}
