9830ad7cf9a7443c3db49f5cbab33eb75c3afca1
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-03 01:38:53 +0900

    [update]レベルエンティティに必要経験値を持たせる

--- Assets/Scripts/Runtime/Entity/Ingame/System/LevelEntity.cs
+++ Assets/Scripts/Runtime/Entity/Ingame/System/LevelEntity.cs
@@ -1,32 +1,61 @@
 using System;
+using UnityEngine;
 
 namespace Cryptos.Runtime.Entity.Ingame.System
 {
+    /// <summary>
+    ///     レベル管理を行うエンティティ
+    /// </summary>
     public class LevelEntity
     {
+        public LevelEntity(float[] requirePoints)
+        {
+            _currentLevel = 1;
+            _levelProgress = 0;
+
+            _requirePoints = requirePoints;
+        }
+
         public event Action<int> OnLevelChanged;
         public event Action<float> OnLevelProgressChanged;
 
         public int CurrentLevel => _currentLevel;
         public float LevelProgress => _levelProgress;
 
+        /// <summary>
+        ///     レベル進行度を増加させます。
+        /// </summary>
+        /// <param name="value"></param>
         public void AddLevelProgress(float value)
         {
-            ChangeLevelProgress(value + _levelProgress);
+            if (_requirePoints.Length <= _currentLevel)
+            {
+                Debug.Log("player is max Level");
+                return;
+            }
+
+            //必要経験値に対する割合を計算
+            float proportion = value / _requirePoints[_currentLevel - 1];
+            //現在の進行度に加算
+            float progress = proportion + _levelProgress;
+
+            ChangeLevelProgress(progress);
         }
 
         private int _currentLevel;
         private float _levelProgress;
 
+        private float[] _requirePoints;
+
         /// <summary>
-        /// レベル進行度を変更する
-        /// 1を超えた場合、レベルアップする
+        ///     レベル進行度を変更します。
+        ///     1を超えた場合、レベルアップします。
         /// </summary>
         /// <param name="value"></param>
         /// <returns></returns>
         private void ChangeLevelProgress(float value)
         {
-            //1以下になるまでレベルアップ処理を行う
+            //進行度が1以下になるまでレベルアップ処理を行う
             while (1 < value)
             {
                 value -= 1;
@@ -38,7 +67,7 @@ namespace Cryptos.Runtime.Entity.Ingame.System
         }
 
         /// <summary>
-        /// レベルを変更する
+        ///     レベルを変更します。
         /// </summary>
         /// <param name="value"></param>
         private void ChangeLevel(int value)
