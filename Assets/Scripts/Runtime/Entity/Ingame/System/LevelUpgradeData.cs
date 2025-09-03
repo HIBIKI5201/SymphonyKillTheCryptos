63c0188e7bbf6aeaf130e6c390de04faf68924b1
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-03 23:10:07 +0900

    [update]仮のstring配列をレベルノードに変更

--- Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeData.cs
+++ Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeData.cs
@@ -6,11 +6,11 @@ namespace Cryptos.Runtime.Entity.Ingame.System
     public class LevelUpgradeData : ScriptableObject
     {
         public float[] LevelRequirePoints => _levelRequirePoints;
-        public string[] LevelCard => _levelCard;
+        public LevelUpgradeNode[] LevelCard => _levelCard;
 
         [SerializeField]
         private float[] _levelRequirePoints = new float[] { 100f, 300f, 600f, 1000f };
         [SerializeField]
-        private string[] _levelCard;
+        private LevelUpgradeNode[] _levelCard;
     }
 }
