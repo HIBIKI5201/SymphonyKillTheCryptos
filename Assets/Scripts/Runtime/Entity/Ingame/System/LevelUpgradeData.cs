f784987dadc0f96be3d35a08d3d4b41a62c99245
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-03 18:32:39 +0900

    [update]レベルアップグレードクラスに変更

--- Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeData.cs
+++ Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeData.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 namespace Cryptos.Runtime.Entity.Ingame.System
 {
-    public class LevelSelectData : ScriptableObject
+    [CreateAssetMenu(fileName = nameof(LevelUpgradeData), menuName = "Cryptos/" + nameof(LevelUpgradeData), order = 0)]
+    public class LevelUpgradeData : ScriptableObject
     {
         public float[] LevelRequirePoints => _levelRequirePoints;
         public string[] LevelCard => _levelCard;
 
         [SerializeField]
         private float[] _levelRequirePoints = new float[] { 100f, 300f, 600f, 1000f };
-
         [SerializeField]
         private string[] _levelCard;
     }
