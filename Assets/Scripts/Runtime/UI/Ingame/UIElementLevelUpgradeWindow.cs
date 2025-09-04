b4eeda18263c1ae6d152dd1f64c3c5840749f6da
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-04 21:18:45 +0900

    [update]ウィンドウをオープン、クローズドするメソッドを追加

--- Assets/Scripts/Runtime/UI/Ingame/UIElementLevelUpgradeWindow.cs
+++ Assets/Scripts/Runtime/UI/Ingame/UIElementLevelUpgradeWindow.cs
@@ -1,3 +1,4 @@
+using Cryptos.Runtime.Presenter;
 using SymphonyFrameWork.Utility;
 using System.Threading.Tasks;
 using UnityEngine.UIElements;
@@ -12,11 +13,46 @@ namespace Cryptos.Runtime.UI.Ingame
 
         }
 
+        public void OnenWindow(LevelUpgradeNodeViewModel[] nodes)
+        {
+            for(int i = 0; i < _nodes.Length; i++)
+            {
+                if(i < nodes.Length)
+                {
+                    LevelUpgradeNodeViewModel node = nodes[i];
+
+                    _nodes[i].SetData(node.Texture, node.NodeName, node.Description);
+                    _nodes[i].style.display = DisplayStyle.Flex;
+                }
+                else // 入力以上のノードは非表示にする
+                {
+                    _nodes[i].style.display = DisplayStyle.None;
+                }
+            }
+
+            style.display = DisplayStyle.Flex;
+        }
+
+        public void CloseWindow()
+        {
+            // とりあえず非表示にするだけ
+            style.display = DisplayStyle.None;
+        }
+
         protected override Task Initialize_S(TemplateContainer container)
         {
+            _nodes = new UIElementLevelUpgradeNode[NODE_MAX];
 
+            for (int i = 0; i < NODE_MAX; i++)
+            {
+                _nodes[i] = container.Q<UIElementLevelUpgradeNode>($"node{i + 1}");
+            }
 
             return Task.CompletedTask;
         }
+
+        private const int NODE_MAX = 3;
+
+        private UIElementLevelUpgradeNode[] _nodes;
     }
 }
