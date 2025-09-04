be2f301e07e0ed9d08ca0f86eda88e0c17776ce5
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-04 21:17:18 +0900

    [add]レベルアップグレードのビューモデルを追加

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/System/LevelUpgradeNodeViewModel.cs
@@ -0,0 +1,19 @@
+using Cryptos.Runtime.Entity.Ingame.System;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Presenter
+{
+    public readonly struct LevelUpgradeNodeViewModel
+    {
+        public LevelUpgradeNodeViewModel(LevelUpgradeNode node)
+        {
+            _node = node;
+        }
+
+        public string NodeName => _node.NodeName;
+        public Texture2D Texture => _node.Texture;
+        public string Description => _node.Description;
+
+        private readonly LevelUpgradeNode _node;
+    }
+}
