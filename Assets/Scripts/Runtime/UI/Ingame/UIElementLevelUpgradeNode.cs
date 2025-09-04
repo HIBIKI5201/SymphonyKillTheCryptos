a2ca07521a174555f98712c0398a5470e304e0a0
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-04 21:17:59 +0900

    [update]データを取得してコンテナに反映するように

--- Assets/Scripts/Runtime/UI/Ingame/UIElementLevelUpgradeNode.cs
+++ Assets/Scripts/Runtime/UI/Ingame/UIElementLevelUpgradeNode.cs
@@ -1,5 +1,6 @@
 using SymphonyFrameWork.Utility;
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Cryptos.Runtime.UI.Ingame
@@ -7,13 +8,29 @@ namespace Cryptos.Runtime.UI.Ingame
     [UxmlElement]
     public partial class UIElementLevelUpgradeNode : SymphonyVisualElement
     {
-        public UIElementLevelUpgradeNode() : base("UIToolKit/UXML/Ingame/LevelUpgradeNode")
+        public UIElementLevelUpgradeNode() : base("UIToolKit/UXML/Ingame/LevelUpgradeNode",
+            0)
         {
+        }
+
+        public void SetData(Texture2D icon, string name, string description)
+        {
+            _iconElement.style.backgroundImage = new StyleBackground(icon);
+            _nameLabel.text = name;
+            _descriptionLabel.text = description;
         }
 
         protected override Task Initialize_S(TemplateContainer container)
         {
+            _iconElement = container.Q<VisualElement>("icon");
+            _nameLabel = container.Q<Label>("name");
+            _descriptionLabel = container.Q<Label>("description");
+
             return Task.CompletedTask;
         }
+
+        private VisualElement _iconElement;
+        private Label _nameLabel;
+        private Label _descriptionLabel;
     }
 }
