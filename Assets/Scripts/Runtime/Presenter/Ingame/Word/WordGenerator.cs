e0a7f1fd73dd7402d1096c823df2f5e9d5d6d19f
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-09 08:13:37 +0900

    [add]UIからワードのビューモデルが取得できるように

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/Word/WordGenerator.cs
@@ -0,0 +1,38 @@
+using Cryptos.Runtime.Entity.Ingame.Word;
+using System;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Presenter.Ingame.Word
+{
+    /// <summary>
+    ///     ワードエンティティを生成する。
+    /// </summary>
+    public static class WordGenerator
+    {
+        public static WordEntityViewModel GetWordEntity(string word = "void")
+        {
+            WordEntity wordEntity = new(word, 1, 1);
+            WordEntityViewModel wordEntityViewModel = new(wordEntity);
+            
+            return wordEntityViewModel;
+        }
+    }
+
+    public struct WordEntityViewModel
+    {
+        public WordEntityViewModel(WordEntity wordEntity)
+        {
+            _entity = wordEntity;
+        }
+
+        public event Action OnComplete
+        {
+            add  => _entity.OnComplete += value;
+            remove => _entity.OnComplete -= value;
+        }
+
+        public void InputChar(char c) => _entity.OnInputChar(c);
+
+        private WordEntity _entity;
+    }
+}
