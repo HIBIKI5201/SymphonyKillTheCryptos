bc79b2566236b79ffad1040d77fc15604b6991ca
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-11 19:18:05 +0900

    [clean]プロパティにサマリーを追加

--- Assets/Scripts/Runtime/Presenter/Ingame/System/LevelUpgradeNodeViewModel.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/System/LevelUpgradeNodeViewModel.cs
@@ -3,17 +3,36 @@ using UnityEngine;
 
 namespace Cryptos.Runtime.Presenter.System
 {
+    /// <summary>
+    /// レベルアップノードのビューモデル。
+    /// </summary>
     public readonly struct LevelUpgradeNodeViewModel
     {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        /// <param name="node">ビューモデルがラップするノード。</param>
         public LevelUpgradeNodeViewModel(LevelUpgradeNode node)
         {
             _node = node;
         }
 
+                /// <summary>
+        /// ノード名。
+        /// </summary>
         public string NodeName => _node.NodeName;
+        /// <summary>
+        /// ノードのテクスチャ。
+        /// </summary>
         public Texture2D Texture => _node.Texture;
+        /// <summary>
+        /// ノードの説明。
+        /// </summary>
         public string Description => _node.Description;
 
+        /// <summary>
+        /// このビューモデルがラップするノード。
+        /// </summary>
         public LevelUpgradeNode LevelUpgradeNode => _node;
 
         private readonly LevelUpgradeNode _node;
