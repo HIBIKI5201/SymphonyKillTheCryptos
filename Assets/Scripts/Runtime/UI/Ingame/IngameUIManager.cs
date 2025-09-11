3c4d06b0defd9a41f1b2c9b9c841934e347b4b17
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-11 19:14:18 +0900

    [clean]サマリーとタイポ修正

--- Assets/Scripts/Runtime/UI/Ingame/IngameUIManager.cs
+++ Assets/Scripts/Runtime/UI/Ingame/IngameUIManager.cs
@@ -12,31 +12,51 @@ using System;
 namespace Cryptos.Runtime.UI.Ingame
 {
     /// <summary>
-    ///     インゲームのUIを管理する
+    ///     インゲームのUIを管理します。
     /// </summary>
     [RequireComponent(typeof(UIDocument))]
     public class IngameUIManager : MonoBehaviour, ICardUIManager, IInitializeAsync
     {
+        /// <summary>
+        /// カードをUIに追加します。
+        /// </summary>
+        /// <param name="instance">追加するカードのビューモデル。</param>
         public void AddCard(CardViewModel instance)
         {
             _deck.HandleAddCard(instance);
         }
 
+        /// <summary>
+        /// カードをUIから削除します。
+        /// </summary>
+        /// <param name="instance">削除するカードのビューモデル。</param>
         public void RemoveCard(CardViewModel instance)
         {
             _deck.HandleRemoveCard(instance);
         }
 
+        /// <summary>
+        /// レベルアップウィンドウを開きます。
+        /// </summary>
+        /// <param name="nodes">表示するノード。</param>
         public void OpenLevelUpgradeWindow(Span<LevelUpgradeNodeViewModel> nodes)
         {
             _levelUpgrade.OpenWindow(nodes);
         }
 
+        /// <summary>
+        /// レベルアップウィンドウを閉じます。
+        /// </summary>
         public void CloseLevelUpgradeWindow()
         {
             _levelUpgrade.CloseWindow();
         }
 
+        /// <summary>
+        /// 選択されたレベルアップノードを取得しようとします。
+        /// </summary>
+        /// <param name="nodeVM">選択されたノードのビューモデル。</param>
+        /// <returns>ノードが選択されていればtrue、そうでなければfalse。</returns>
         public bool TryGetSelectedLevelUpgradeNode(out LevelUpgradeNodeViewModel nodeVM)
         {
             UIElementLevelUpgradeNode node = _levelUpgrade.GetSelectedLevelUpgrade();
@@ -47,7 +67,11 @@ namespace Cryptos.Runtime.UI.Ingame
             return !isNull;
         }
 
-        public void OnInutChar(char c)
+        /// <summary>
+        /// 文字入力を処理します。
+        /// </summary>
+        /// <param name="c">入力された文字。</param>
+        public void OnInputChar(char c)
         {
             _levelUpgrade.InputChar(c);
         }
