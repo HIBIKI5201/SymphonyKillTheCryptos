6af3d9d08d60b973734dd6d073a52e65f15d5ac3
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-13 02:40:13 +0900

    [update]命名を変更

--- Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeNode/LevelUpgradeStatusEffect.cs
+++ Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeNode/LevelUpgradeStatusEffect.cs
@@ -2,7 +2,7 @@ using Cryptos.Runtime.Entity.Ingame.Character;
 
 namespace Cryptos.Runtime.Entity
 {
-    public abstract class ILevelUpgradeStatusEffect : ILevelUpgradeEffect
+    public abstract class LevelUpgradeStatusEffect : ILevelUpgradeEffect
     {
         public abstract void ApplyStatusEffect(TentativeCharacterData target);
     }
