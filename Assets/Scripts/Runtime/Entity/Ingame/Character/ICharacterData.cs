8f85f7469899951543a6affc0247404a89b28ea1
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-13 02:44:07 +0900

    [add]キャラクターのデータのインターフェースを追加

---
+++ Assets/Scripts/Runtime/Entity/Ingame/Character/ICharacterData.cs
@@ -0,0 +1,9 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Entity
+{
+    public interface ICharacterData : IAttackableData, IHittableData
+    {
+    }
+}
