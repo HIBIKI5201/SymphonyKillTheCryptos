613be67bd4cf599f497a13ea890baa56578b1212
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-13 08:33:57 +0900

    [update]コンストラクタからデータを受け取るように

--- Assets/Scripts/Runtime/Entity/Ingame/Card/CardData.cs
+++ Assets/Scripts/Runtime/Entity/Ingame/Card/CardData.cs
@@ -6,13 +6,31 @@ using UnityEngine;
 namespace Cryptos.Runtime.Entity.Ingame.Card
 {
     /// <summary>
-    /// ゲーム内の「カード」の静的なデータ情報を持つScriptableObject。
+    /// ゲーム内のカードの静的なデータ情報を持つクラス。
     /// カードの名前、説明、難易度、アイコン、ワードの範囲、およびカード効果の配列を定義します。
     /// </summary>
-    [CreateAssetMenu(fileName = nameof(CardData),
-        menuName = CryptosPathConstant.ASSET_PATH + nameof(CardData))]
     public class CardData : ScriptableObject
     {
+        public CardData(
+            string cardName,
+            string cardExplanation,
+            Texture2D cardIcon,
+            int cardDifficulty,
+            Vector2Int wordRange,
+            CardContents[] contentsArray,
+            int priority,
+            int animationClipID)
+        {
+            _cardName = cardName;
+            _cardExplanation = cardExplanation;
+            _cardIcon = cardIcon;
+            _cardDifficulty = cardDifficulty;
+            _wordRange = wordRange;
+            _contentsArray = contentsArray;
+            _priority = priority;
+            _animationClipID = animationClipID;
+        }
+
         /// <summary>
         /// カードの名前を取得します。
         /// </summary>
@@ -24,14 +42,14 @@ namespace Cryptos.Runtime.Entity.Ingame.Card
         public string CardExplanation => _cardExplanation;
 
         /// <summary>
-        /// カードの難易度を取得します。
-        /// </summary>
-        public int CardDifficulty => _cardDifficulty;
-
-        /// <summary>
         /// カードのアイコン画像を取得します。
         /// </summary>
         public Texture2D CardIcon => _cardIcon;
+
+        /// <summary>
+        /// カードの難易度を取得します。
+        /// </summary>
+        public int CardDifficulty => _cardDifficulty;
 
         /// <summary>
         /// ワードデータベースからワードを選択する範囲（x:開始インデックス, y:終了インデックス）を取得します。
@@ -53,31 +71,13 @@ namespace Cryptos.Runtime.Entity.Ingame.Card
         /// </summary>
         public int AnimationClipID => _animationClipID;
 
-
-        [Header("基本情報")]
-        [SerializeField, Tooltip("カードの名前")]
         private string _cardName = "empty";
-
-        [SerializeField, Tooltip("カードの説明"), TextArea]
         private string _cardExplanation = "explanation";
-
-        [SerializeField, Tooltip("アイコン画像")]
         private Texture2D _cardIcon = default;
-
-        [Header("カードの詳細設定")]
-        [SerializeField, Tooltip("カードの難易度"), Min(1)]
         private int _cardDifficulty = 1;
-
-        [SerializeField, Tooltip("ワードの範囲")]
         private Vector2Int _wordRange = new Vector2Int(0, 1);
-
-        [SerializeField, Tooltip("このカードが持つ効果の配列。")]
         private CardContents[] _contentsArray = default;
-        [SerializeField, Tooltip("カード優先度")]
         private int _priority = 0;
-
-        [Header("演出設定")]
-        [SerializeField, Tooltip("アニメーションのID")]
         private int _animationClipID = default;
 
         /// <summary>
