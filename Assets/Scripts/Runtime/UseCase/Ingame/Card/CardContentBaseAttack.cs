307ffa15730aa16b2fc95c0fdd91d3ee82165ac7
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-13 09:35:07 +0900

    [update]ベースクラスを実装

--- Assets/Scripts/Runtime/UseCase/Ingame/Card/CardContentBaseAttack.cs
+++ Assets/Scripts/Runtime/UseCase/Ingame/Card/CardContentBaseAttack.cs
@@ -1,16 +1,21 @@
-using UnityEngine;
+using Cryptos.Runtime.Entity.Ingame.Card;
+using Cryptos.Runtime.Entity.Ingame.Character;
+using Cryptos.Runtime.UseCase.Ingame.CombatSystem;
+using System.Linq;
 
-public class CardContentBaseAttack : MonoBehaviour
+namespace Cryptos.Runtime.UseCase.Ingame.Card
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public abstract class CardContentBaseAttack : ICardContent
     {
-        
-    }
+        public virtual void InitializeCombatHandler(ICombatHandler[] handlers)
+        {
+            ICombatHandler[] myHandlers = GetMyCombatHandler();
+            _handlers = handlers.Concat(myHandlers).ToArray();
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public abstract void Execute(ICharacter[] players, ICharacter[] targets);
+
+        protected ICombatHandler[] _handlers;
+        protected abstract ICombatHandler[] GetMyCombatHandler();
     }
 }
