edf40f48f01132a71df90bef0be2e8d891f4f7ed
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-13 22:47:26 +0900

    [add]アーマー増加アップグレードを追加

---
+++ Assets/Scripts/Runtime/Entity/Ingame/System/LevelUpgradeNode/ArmorStatusUpgrade.cs
@@ -0,0 +1,16 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Entity.Ingame.System
+{
+    public class ArmorStatusUpgrade : LevelUpgradeStatusEffect
+    {
+        public override void ApplyStatusEffect(TentativeCharacterData target)
+        {
+            target.SetNewBuff(TentativeCharacterData.BuffType.Armor,
+                _armorIncreaseAmount);
+        }
+
+        [SerializeField, Min(0), Tooltip("装甲増加量。(%)")] private float _armorIncreaseAmount = 0;
+    }
+}
