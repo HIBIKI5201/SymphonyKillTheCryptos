29fc55027b8e6333a25d519561179318257e0478
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-15 02:00:01 +0900

    [add]ダメージテキストのプールを追加

---
+++ Assets/Scripts/Runtime/UI/Ingame/DamageTextPool.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UIElements;
+
+namespace Cryptos.Runtime.UI
+{
+    public class DamageTextPool
+    {
+        public DamageTextPool(VisualElement root, Vector3 offset)
+        {
+            _pool = new(
+                createFunc: () => new UIElementDamageText(),
+                actionOnGet: t => root.Add(t),
+                actionOnRelease: t => root.Remove(t)
+                );
+        }
+
+        public async void ShowDamageText(float damage, Vector3 position, float duration = 1)
+        {
+            UIElementDamageText text = _pool.Get();
+            text.SetData(damage, position);
+
+            await Awaitable.WaitForSecondsAsync(duration);
+
+            _pool.Release(text);
+        }
+
+        private ObjectPool<UIElementDamageText> _pool;
+    }
+}
