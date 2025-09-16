4a2e5af7c48e7afb8f1f1f847dfe417a86a9a9a0
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-16 11:45:58 +0900

    [update]銃を表示・非表示するメソッドを追加

--- Assets/Scripts/Runtime/UI/Ingame/AnimeManager/FirearmAnimeManager.cs
+++ Assets/Scripts/Runtime/UI/Ingame/AnimeManager/FirearmAnimeManager.cs
@@ -13,9 +13,27 @@ namespace Cryptos.Runtime.UI.Ingame.Character.Player
             _onFire.Invoke();
         }
 
+        public void Show()
+        {
+            if (_visible) return;
+
+            _visible = true;
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            if (!_visible) return;
+
+            _visible = false;
+            gameObject.SetActive(false);
+        }
+
         [SerializeField]
         private UnityEvent _onFire;
         [SerializeField]
         private CriAtomSource _source;
+
+        private bool _visible;
     }
 }
