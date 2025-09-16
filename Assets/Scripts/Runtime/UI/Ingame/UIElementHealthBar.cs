0eaa2f0c8d32d9c6fa821dc16fde931285391b1e
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-16 10:15:23 +0900

    [update]位置のトラッキングメソッドを追加

--- Assets/Scripts/Runtime/UI/Ingame/UIElementHealthBar.cs
+++ Assets/Scripts/Runtime/UI/Ingame/UIElementHealthBar.cs
@@ -14,7 +14,46 @@ namespace Cryptos.Runtime.UI.Ingame
 
         }
 
-        public void GuageChange(float value, float maxValue)
+        public void RegisterTarget()
+        {
+
+        }
+
+        protected override Task Initialize_S(TemplateContainer container)
+        {
+            _base = container.Q(BASE_NAME);
+            _redBar = container.Q(RED_BAR_NAME);
+            _greenBar = container.Q(GREEN_BAR_NAME);
+
+            return Task.CompletedTask;
+        }
+
+        private const string BASE_NAME = "base";
+        private const string RED_BAR_NAME = "red-bar";
+        private const string GREEN_BAR_NAME = "green-bar";
+
+        private const float GREEN_DURATION = 0.5f;
+        private const float RED_DURATION = 0.8f;
+        private const float DERAY_BETWEEN = 1.5f;
+
+        private VisualElement _base;
+        private VisualElement _redBar;
+        private VisualElement _greenBar;
+
+        private Transform _trackingTarget;
+        private float _lastValue;
+        private CancellationTokenSource _cts;
+
+        private async void RegisterTrackingTarget(Transform transform)
+        {
+            while (transform != null)
+            {
+                MovePosition(transform.position);
+                await Awaitable.NextFrameAsync();
+            }
+        }
+
+        private void GuageChange(float value, float maxValue)
         {
             float proportion = _lastValue / value;
 
@@ -37,26 +76,6 @@ namespace Cryptos.Runtime.UI.Ingame
             _lastValue = value;
         }
 
-        protected override Task Initialize_S(TemplateContainer container)
-        {
-            _redBar = container.Q(RED_BAR_NAME);
-            _greenBar = container.Q(GREEN_BAR_NAME);
-
-            return Task.CompletedTask;
-        }
-
-        private const string RED_BAR_NAME = "red-bar";
-        private const string GREEN_BAR_NAME = "green-bar";
-
-        private const float GREEN_DURATION = 0.5f;
-        private const float RED_DURATION = 0.8f;
-        private const float DERAY_BETWEEN = 1.5f;
-
-        private VisualElement _redBar;
-        private VisualElement _greenBar;
-
-        private float _lastValue;
-        private CancellationTokenSource _cts;
 
         private async void DamageEffect(float proportion, CancellationToken token)
         {
@@ -83,7 +102,7 @@ namespace Cryptos.Runtime.UI.Ingame
 
                 await redTweenTask;
             }
-            catch(OperationCanceledException) { return; }
+            catch (OperationCanceledException) { return; }
         }
 
         private void HealEffect(float proportion)
@@ -92,6 +111,28 @@ namespace Cryptos.Runtime.UI.Ingame
 
             _greenBar.style.width = length;
             _redBar.style.height = length;
+        }
+
+        private void MovePosition(Vector3 position)
+        {
+            if (_base.panel == null) return;
+
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(position);
+            Vector2 center = new(screenPos.x, screenPos.y);
+
+            //背景の中心座標。
+            Vector2 size = new Vector2(
+                _base.resolvedStyle.width,
+                _base.resolvedStyle.height)
+                / 2;
+
+            Vector2 selfPivot = center - size;
+
+            //UITK座標系では値が高いほど下に移動する
+            selfPivot = new(selfPivot.x, Screen.height - selfPivot.y);
+
+            style.width = selfPivot.x;
+            style.height = selfPivot.y;
         }
     }
 }
