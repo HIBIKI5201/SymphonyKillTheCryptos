65f5ff99804250d7af1d2f73401bb4eff9f5607e
HIBIKI_5201 <takuto5201@gmail.com>
2025-09-16 10:32:08 +0900

    [update]ヘルスバーのVMを追加

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/Character/HealthBarViewModel.cs
@@ -0,0 +1,36 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using System;
+using System.Threading;
+using UnityEngine;
+
+namespace Cryptos.Runtime.Presenter
+{
+    public readonly struct HealthBarViewModel
+    {
+        public HealthBarViewModel(CharacterEntity entity, Transform transform, CancellationToken token)
+        {
+            _entity = entity;
+            _trackingTarget = transform;
+            _token = token;
+        }
+
+        public event Action<float, float> OnHealthChaged
+        {
+            add => _entity.OnHealthChanged += value;
+            remove => _entity.OnHealthChanged -= value;
+        }
+
+        public event Action OnDead
+        {
+            add => _entity.OnDead += value;
+            remove => _entity.OnDead -= value;
+        }
+
+        public Transform TrackingTarget => _trackingTarget;
+        public CancellationToken Token => _token;
+
+        private readonly CharacterEntity _entity;
+        private readonly Transform _trackingTarget;
+        private readonly CancellationToken _token;
+    }
+}
