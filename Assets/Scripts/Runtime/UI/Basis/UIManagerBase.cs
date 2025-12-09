a51f3517711a984d36a2877574bda3739d7f1dde
HIBIKI_5201 <takuto5201@gmail.com>
2025-12-09 14:05:36 +0900

    [add]UIマネージャーのベースクラスを追加

---
+++ Assets/Scripts/Runtime/UI/Basis/UIManagerBase.cs
@@ -0,0 +1,23 @@
+using SymphonyFrameWork;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Cryptos.Runtime.Presenter
+{
+    [RequireComponent(typeof(UIDocument))]
+    public abstract class UIManagerBase : MonoBehaviour, IInitializeAsync
+    {
+        Task IInitializeAsync.InitializeTask { get; set; }
+
+        async Task IInitializeAsync.InitializeAsync()
+        {
+            _document = GetComponent<UIDocument>();
+            await InitializeDocumentAsync(_document, _document.rootVisualElement);
+        }
+
+        protected UIDocument _document;
+
+        protected abstract Task InitializeDocumentAsync(UIDocument document, VisualElement root);
+    }
+}
