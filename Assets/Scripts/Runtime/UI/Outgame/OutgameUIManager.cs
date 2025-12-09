989db6d14a130d42fe502152a5c2f06542b9ccb7
HIBIKI_5201 <takuto5201@gmail.com>
2025-12-09 16:27:53 +0900

    [update]イベント登録方法をRegistartCallBackに変更

--- Assets/Scripts/Runtime/UI/Outgame/OutgameUIManager.cs
+++ Assets/Scripts/Runtime/UI/Outgame/OutgameUIManager.cs
@@ -1,6 +1,7 @@
 using Cryptos.Runtime.UI.Basis;
 using System;
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Cryptos.Runtime.UI.Outgame
@@ -10,8 +11,8 @@ namespace Cryptos.Runtime.UI.Outgame
         /// <summary> スタートボタンが押された時 </summary>
         public event Action OnPressedStartButton
         {
-            add  => _startButton.clicked += value;
-            remove => _startButton.clicked -= value;
+            add => _onStartButtonPressed += value;
+            remove => _onStartButtonPressed -= value;
         }
 
         protected override async Task InitializeDocumentAsync(UIDocument document, VisualElement root)
@@ -21,12 +22,25 @@ namespace Cryptos.Runtime.UI.Outgame
 
             if (_startButton == null) { return; }
 
+            _startButton.RegisterCallback<NavigationSubmitEvent>(HandleStartButtonPressed);
             _startButton.Focus(); // 初期状態はスタートボタンをフォーカスする。
+
+            destroyCancellationToken.Register(() =>
+            {
+                _startButton.UnregisterCallback<NavigationSubmitEvent>(HandleStartButtonPressed);
+            });
+
+            void HandleStartButtonPressed(NavigationSubmitEvent evt)
+            {
+                _onStartButtonPressed?.Invoke();
+            }
         }
 
         private const string BUTTONS_NAME = "buttons";
         private const string START_BUTTON_NAME = "start";
 
         private Button _startButton;
+
+        private event Action _onStartButtonPressed;
     }
 }
