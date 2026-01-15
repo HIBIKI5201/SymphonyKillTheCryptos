dd441a31d3f26dd0d855dd9eaec49cbbca2cc818
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-15 20:12:25 +0900

    [add]インゲームのUIマネージャーのハンドラ

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/System/IIngameUIManager.cs
@@ -0,0 +1,8 @@
+namespace Cryptos.Runtime.Presenter.Ingame.System
+{
+    public interface IIngameUIManager
+    {
+        public void OnLevelUpgradeInputChar(char c);
+        public void CardInputChar(char c);
+    }
+}
