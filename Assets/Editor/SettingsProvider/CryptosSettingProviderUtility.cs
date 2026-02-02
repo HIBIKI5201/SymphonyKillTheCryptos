4cf1a46f428d973a2f20741313ac19ce7f0b0133
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-03 03:29:55 +0900

    [add]コンフィグパスを追加

--- Assets/Editor/SettingsProvider/CryptosSettingProviderUtility.cs
+++ Assets/Editor/SettingsProvider/CryptosSettingProviderUtility.cs
@@ -1,12 +1,13 @@
-using Unity.VisualScripting;
 using UnityEditor;
 
 namespace Cryptos.Editor.ProjectSetting
 {
     public static class CryptosSettingProviderUtility
     {
+        public const string EDITOR_CONFIG_PAHT = "ProjectSettings/Cryptos/";
+
         public static string GetSettingPath<T>()
             where T : SettingsProvider
-            => $"Cryptos/{typeof(T)}";
+            => $"Cryptos/{typeof(T).Name}";
     }
 }
