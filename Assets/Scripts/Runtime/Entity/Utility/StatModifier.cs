c60a508fe3f5f4028963f068b29094fcb7e32583
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-02 23:37:18 +0900

    [add]モディファイア専用構造体を追加

---
+++ Assets/Scripts/Runtime/Entity/Utility/StatModifier.cs
@@ -0,0 +1,50 @@
+
+namespace Cryptos.Runtime.Entity.Utility
+{
+    /// <summary>
+    /// ステータス変更の種類を定義します。
+    /// </summary>
+    public enum StatModType
+    {
+        /// <summary>
+        /// 加算
+        /// </summary>
+        Additive,
+        /// <summary>
+        /// 乗算 (%)
+        /// </summary>
+        Multiplier
+    }
+
+    /// <summary>
+    /// ステータスへの変更を表すイミュータブルなクラス。
+    /// </summary>
+    public struct StatModifier
+    {
+        public float Value => _value;
+        public StatModType Type => _type;
+        public int Priority => _priority;
+        public object Source => _source;
+
+        /// <summary>
+        ///     コンストラクタ。
+        /// </summary>
+        /// <param name="value">変更する値。</param>
+        /// <param name="type">変更の種類（加算または乗算）。</param>
+        /// <param name="priority">乗算の場合の優先度。</param>
+        /// <param name="source">このModifierの出処（デバッグや特定のModifier削除用）。</param>
+        public StatModifier(float value, StatModType type, int priority = 0, object source = null)
+        {
+            _value = value;
+            _type = type;
+            _priority = priority;
+            _source = source;
+        }
+
+        private readonly float _value;
+        private StatModType _type;
+        private int _priority;
+        private object _source;
+
+    }
+}
