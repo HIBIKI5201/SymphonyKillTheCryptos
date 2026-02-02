48e3674ac2a0a035d5c38f3e39365474cc35ecbd
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-02 23:39:44 +0900

    [update]レベルのユースケースをエンティティを使用したものに変更

--- Assets/Scripts/Runtime/UseCase/Ingame/System/LevelUseCase.cs
+++ Assets/Scripts/Runtime/UseCase/Ingame/System/LevelUseCase.cs
@@ -48,14 +48,7 @@ namespace Cryptos.Runtime.UseCase.Ingame.System
             if (selectedNode == null) return;
 
             // 選択されたノードを記録します。
-            if (_acquiredUpgrades.ContainsKey(selectedNode))
-            {
-                _acquiredUpgrades[selectedNode]++;
-            }
-            else
-            {
-                _acquiredUpgrades[selectedNode] = 1;
-            }
+            _acquiredUpgradeEntity.Add(selectedNode);
 
             // 選択されたノードの効果を適用します。
             foreach (var effect in selectedNode.Effects)
@@ -70,8 +63,8 @@ namespace Cryptos.Runtime.UseCase.Ingame.System
         private readonly LevelEntity _levelEntity;
         private readonly LevelUpgradeData _data;
         private readonly TentativeCharacterData _characterData;
+        private readonly AcquiredUpgradeEntity _acquiredUpgradeEntity = new();
         private readonly Queue<int> _levelUpQueue = new();
-        private readonly Dictionary<LevelUpgradeNode, int> _acquiredUpgrades = new();
 
         /// <summary>
         ///     レベルアップのノード選択を待機します。
@@ -81,8 +74,7 @@ namespace Cryptos.Runtime.UseCase.Ingame.System
         {
             // 取得回数が上限に達していないノードを候補とする。
             LevelUpgradeNode[] allNode = _data.LevelCard
-                .Where(node => 
-                    !_acquiredUpgrades.TryGetValue(node, out int count) || count < node.MaxStack)
+                .Where(node => _acquiredUpgradeEntity.GetCount(node) < node.MaxStack)
                 .ToArray();
             
             int amount = _data.LevelUpgradeAmount;
