eee8e8d498e9154c5e3b12b047d5daa32e452b9c
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-06 14:46:49 +0900

    [add]レベルアップのプレゼンタークラスを追加

---
+++ Assets/Scripts/Runtime/Presenter/Ingame/System/LevelUpPresenter.cs
@@ -0,0 +1,33 @@
+using Cryptos.Runtime.UseCase.Ingame.System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cryptos.Runtime.Presenter.Ingame.System
+{
+    public class LevelUpPresenter
+    {
+        private readonly LevelUseCase _levelUseCase;
+        private readonly ILevelUpUIManager _levelUpUIManager;
+
+        public LevelUpPresenter(LevelUseCase levelUseCase, ILevelUpUIManager levelUpUIManager)
+        {
+            _levelUseCase = levelUseCase;
+            _levelUpUIManager = levelUpUIManager;
+        }
+
+        public async Task<LevelUpgradeOption> HandleLevelUpSelectAsync(LevelUpgradeOption[] options)
+        {
+            // LevelUpgradeOption[] を LevelUpgradeNodeViewModel[] に変換
+            var nodeViewModels = options.Select(o => new LevelUpgradeNodeViewModel(o.OriginalNode)).ToArray();
+            // 現在のプレイヤーレベルを取得
+            int currentPlayerLevel = _levelUseCase.GetCurrentLevel();
+            // LevelUpScreenViewModel を生成
+            var screenViewModel = new LevelUpScreenViewModel(nodeViewModels, currentPlayerLevel);
+
+            // IngameUIManager に LevelUpScreenViewModel を渡す
+            var selectedNodeVM = await _levelUpUIManager.LevelUpSelectAsync(screenViewModel);
+            // 選択された LevelUpgradeNodeViewModel に含まれる LevelUpgradeNode を元に新しい LevelUpgradeOption を生成して返す
+            return new LevelUpgradeOption(selectedNodeVM.LevelUpgradeNode);
+        }
+    }
+}
