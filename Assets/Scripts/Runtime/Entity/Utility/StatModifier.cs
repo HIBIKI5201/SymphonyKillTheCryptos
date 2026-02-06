a3e46ecc3a1766eb2f9ff2a77d4fbb36016b4f04
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-07 01:14:00 +0900

    [fix]不要なソースを削除

--- Assets/Scripts/Runtime/Entity/Utility/StatModifier.cs
+++ Assets/Scripts/Runtime/Entity/Utility/StatModifier.cs
@@ -19,12 +19,11 @@ namespace Cryptos.Runtime.Entity.Utility
     /// <summary>
     /// ステータスへの変更を表すイミュータブルなクラス。
     /// </summary>
-    public struct StatModifier
+    public class StatModifier
     {
         public float Value => _value;
         public StatModType Type => _type;
         public int Priority => _priority;
-        public object Source => _source;
 
         /// <summary>
         ///     コンストラクタ。
@@ -32,19 +31,16 @@ namespace Cryptos.Runtime.Entity.Utility
         /// <param name="value">変更する値。</param>
         /// <param name="type">変更の種類（加算または乗算）。</param>
         /// <param name="priority">乗算の場合の優先度。</param>
-        /// <param name="source">このModifierの出処（デバッグや特定のModifier削除用）。</param>
-        public StatModifier(float value, StatModType type, int priority = 0, object source = null)
+        public StatModifier(float value, StatModType type, int priority = 0)
         {
             _value = value;
             _type = type;
             _priority = priority;
-            _source = source;
         }
 
         private readonly float _value;
         private StatModType _type;
         private int _priority;
-        private object _source;
 
     }
 }
