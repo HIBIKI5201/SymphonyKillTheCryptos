176cf0774470bf107c04e8fad799e801d5fc978b
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-08 04:37:40 +0900

    [update]スコアのテキストを取得

--- Assets/Scripts/Runtime/UI/Ingame/HUD/UIElementResultWindow.cs
+++ Assets/Scripts/Runtime/UI/Ingame/HUD/UIElementResultWindow.cs
@@ -1,3 +1,4 @@
+using Cryptos.Runtime.Presenter.Ingame.System;
 using System;
 using System.Threading.Tasks;
 using UnityEngine.UIElements;
@@ -14,17 +15,20 @@ namespace Cryptos.Runtime.UI.Ingame.Manager
         protected override async ValueTask Initialize_S(VisualElement root)
         {
             _resultTitle = root.Q<Label>(RESULT_TITLE_NAME);
-            _scoreText = root.Q<Label>(SCORE_TEXT_NAME);
+            _scoreWaveText = root.Q<Label>(SCORE_TEXT_WAVE_NAME);
+            _scoreTimeText = root.Q<Label>(SCORE_TEXT_TIME_NAME);
+            _scoreSkillText = root.Q<Label>(SCORE_TEXT_SKILL_NAME);
             _returnButton = root.Q<Button>(RETURN_BUTTON_NAME);
 
             _returnButton.clicked += () => OnReturnButtonClicked?.Invoke();
             await Task.CompletedTask;
         }
 
-        public void SetResult(string title, int score)
+        public void SetResult(ResultViewModel vm)
         {
-            _resultTitle.text = title;
-            _scoreText.text = $"Score: {score}";
+            _scoreWaveText.text = $"wave: {vm.Wave}";
+            _scoreTimeText.text = $"time: {vm.Time}";
+            _scoreSkillText.text = $"Skill Point: {vm.SkillPoint}";
         }
 
         public void Open()
@@ -40,11 +44,15 @@ namespace Cryptos.Runtime.UI.Ingame.Manager
 
         private const string UXML_PATH = "ResultWindow";
         private const string RESULT_TITLE_NAME = "result-title";
-        private const string SCORE_TEXT_NAME = "score-text";
+        private const string SCORE_TEXT_WAVE_NAME = "score-text-wave";
+        private const string SCORE_TEXT_TIME_NAME = "score-text-time";
+        private const string SCORE_TEXT_SKILL_NAME = "score-text-skill";
         private const string RETURN_BUTTON_NAME = "return-button";
 
         private Label _resultTitle;
-        private Label _scoreText;
+        private Label _scoreWaveText;
+        private Label _scoreTimeText;
+        private Label _scoreSkillText;
         private Button _returnButton;
     }
 }
