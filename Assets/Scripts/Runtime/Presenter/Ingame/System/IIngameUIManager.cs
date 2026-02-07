d1fe74f2cd4c694338df276ed746aa4d16642870
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-07 18:22:37 +0900

    [update]リザルトを開くメソッドを追加

--- Assets/Scripts/Runtime/Presenter/Ingame/System/IIngameUIManager.cs
+++ Assets/Scripts/Runtime/Presenter/Ingame/System/IIngameUIManager.cs
@@ -1,8 +1,11 @@
+using System;
+
 namespace Cryptos.Runtime.Presenter.Ingame.System
 {
     public interface IIngameUIManager
     {
         public void OnLevelUpgradeInputChar(char c);
         public void CardInputChar(char c);
+        public void OpenResultWindow(string title, int score, Action onReturnButtonClicked);
     }
 }
