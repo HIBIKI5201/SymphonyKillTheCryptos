731b1cb8082042df14e3ef428e6e5dc49cbbd42c
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-08 16:09:58 +0900

    [add]デッキのクラスを作成

---
+++ Assets/Scripts/Runtime/Entity/Ingame/Card/CardDeckEntity.cs
@@ -0,0 +1,15 @@
+namespace Cryptos.Runtime.Entity.Ingame.Card
+{
+    public class CardDeckEntity
+    {
+        public CardDeckEntity(CardData[] cards)
+        {
+            _cards = cards;
+        }
+
+        public CardData this[int index] => _cards[index];
+        public CardData[] CardData => _cards;
+
+        private readonly CardData[] _cards;
+    }
+}
