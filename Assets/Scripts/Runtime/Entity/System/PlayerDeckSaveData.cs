cf3be9cba11fd5c7ae6c3268e2fb14723b29508f
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-10 14:57:44 +0900

    [update]デッキのデータを返すようにする

--- Assets/Scripts/Runtime/Entity/System/PlayerDeckSaveData.cs
+++ Assets/Scripts/Runtime/Entity/System/PlayerDeckSaveData.cs
@@ -1,4 +1,3 @@
-using Cryptos.Runtime.Entity.Ingame.Card;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,17 +7,47 @@ namespace Cryptos.Runtime.Entity
     [Serializable]
     public class PlayerDeckSaveData
     {
+        public PlayerDeckSaveData()
+        {
+            _attackDeck = new Dictionary<string, CardAddressValueObject[]>();
+
+        }
+
+
+
+        /// <summary>
+        ///     デッキを登録または更新する。
+        /// </summary>
+        /// <param name="deckName">登録するデッキの名前。</param>
+        /// <param name="deck">登録するカードアドレスの配列。</param>
         public void RegisterDeck(string deckName, CardAddressValueObject[] deck)
         {
-            if (_attackDeck.ContainsKey(deckName))
-            {
-                _attackDeck[deckName] = deck;
-            }
-            else
-            {
-                throw new Exception($"{deckName} Deck is null");
-            }
+            _attackDeck[deckName] = deck;
         }
+
+        /// <summary>
+        ///     指定された名前のデッキを取得する。
+        /// </summary>
+        /// <param name="deckName">取得するデッキの名前。</param>
+        /// <returns>指定された名前のカードアドレスの配列。存在しない場合はnullを返す。</returns>
+        public CardAddressValueObject[] GetDeck(string deckName)
+        {
+            _attackDeck.TryGetValue(deckName, out CardAddressValueObject[] deck);
+            return deck;
+        }
+
+        /// <summary>
+        ///     保存されているすべてのデッキの名前を取得する。
+        /// </summary>
+        /// <returns>保存されているデッキの名前のコレクション。</returns>
+        public IReadOnlyCollection<string> GetAllDeckNames()
+        {
+            return _attackDeck.Keys;
+        }
+
+
+
+
 
         [SerializeField]
         private Dictionary<string, CardAddressValueObject[]> _attackDeck;
