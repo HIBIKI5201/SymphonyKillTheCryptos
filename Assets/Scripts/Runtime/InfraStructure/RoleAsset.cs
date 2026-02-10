5e27a29d0921a0131aa2a0bd8596f18c5878c9cf
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-10 12:01:42 +0900

    [add]役職のアセットを追加

---
+++ Assets/Scripts/Runtime/InfraStructure/RoleAsset.cs
@@ -0,0 +1,23 @@
+using Cryptos.Runtime.Entity.Ingame.Character;
+using Cryptos.Runtime.Framework;
+using Cryptos.Runtime.InfraStructure.Ingame.DataAsset;
+using UnityEngine;
+
+namespace Cryptos.Runtime.InfraStructure
+{
+    [CreateAssetMenu(fileName = nameof(RoleAsset),
+        menuName = CryptosPathConstant.ASSET_PATH + nameof(RoleAsset))]
+    public class RoleAsset : ScriptableObject
+    {
+        public string Name => _name;
+        public CardDeckAsset Deck => _deck;
+        public CharacterData Status => _status;
+
+        [SerializeField]
+        private string _name;
+        [SerializeField]
+        private CardDeckAsset _deck;
+        [SerializeField]
+        private CharacterData _status;
+    }
+}
