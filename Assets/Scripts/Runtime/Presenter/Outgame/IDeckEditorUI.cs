f22b16ba876d9e02f78fa5340702f48b48a56979
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-10 15:03:55 +0900

    [add]デッキのデッキエディタのインターフェースを追加

---
+++ Assets/Scripts/Runtime/Presenter/Outgame/IDeckEditorUI.cs
@@ -0,0 +1,43 @@
+using Cryptos.Runtime.Entity.Ingame.Card; // For DeckCardEntity
+using Cryptos.Runtime.Presenter.OutGame; // For DeckViewModel
+using System.Collections.Generic;
+using UnityEngine.UIElements; // Add this for SetSelectedDeckInDropdown's internal workings if needed by UI implementation
+
+namespace Cryptos.Runtime.Presenter.OutGame
+{
+    /// <summary>
+    ///     デッキエディタUIが実装すべきインターフェースである。
+    /// </summary>
+    public interface IDeckEditorUI
+    {
+        /// <summary>
+        ///     デッキ名のリストを表示する。
+        /// </summary>
+        /// <param name="deckNames">表示するデッキ名のコレクション。</param>
+        void ShowDeckNames(IReadOnlyCollection<string> deckNames);
+
+        /// <summary>
+        ///     ドロップダウンで選択されているデッキを設定する。
+        /// </summary>
+        /// <param name="deckName">選択するデッキの名前。</param>
+        void SetSelectedDeckInDropdown(string deckName);
+
+        /// <summary>
+        ///     指定されたデッキを表示する。
+        /// </summary>
+        /// <param name="deckViewModel">表示するデッキのViewModel。</param>
+        void ShowDeck(DeckViewModel deckViewModel);
+
+        /// <summary>
+        ///     エラーメッセージを表示する。
+        /// </summary>
+        /// <param name="message">表示するエラーメッセージ。</param>
+        void ShowErrorMessage(string message);
+
+        /// <summary>
+        ///     成功メッセージを表示する。
+        /// </summary>
+        /// <param name="message">表示する成功メッセージ。</param>
+        void ShowSuccessMessage(string message);
+    }
+}
