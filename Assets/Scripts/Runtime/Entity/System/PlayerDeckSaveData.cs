fbf7c7358633b40fcade7784e32d8c14f4b3d533
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-10 12:02:01 +0900

    [update]デッキのセーブデータを更新

--- Assets/Scripts/Runtime/Entity/System/PlayerDeckSaveData.cs
+++ Assets/Scripts/Runtime/Entity/System/PlayerDeckSaveData.cs
@@ -8,6 +8,18 @@ namespace Cryptos.Runtime.Entity
     [Serializable]
     public class PlayerDeckSaveData
     {
+        public void RegisterDeck(string deckName, CardAddressValueObject[] deck)
+        {
+            if (_attackDeck.ContainsKey(deckName))
+            {
+                _attackDeck[deckName] = deck;
+            }
+            else
+            {
+                throw new Exception($"{deckName} Deck is null");
+            }
+        }
+
         [SerializeField]
         private Dictionary<string, CardAddressValueObject[]> _attackDeck;
     }
