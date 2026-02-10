5761e373415938792105b8262a2ae013076399d3
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-10 11:52:16 +0900

    [update]比較演算を追加

--- Assets/Scripts/Runtime/Entity/System/CardAddressValueObject.cs
+++ Assets/Scripts/Runtime/Entity/System/CardAddressValueObject.cs
@@ -1,10 +1,28 @@
+using System;
+
 namespace Cryptos.Runtime.Entity
 {
+    [Serializable]
     public readonly struct CardAddressValueObject
     {
         public CardAddressValueObject(string value) => _value = value;
 
         public string Value => _value;
+
+        public bool Equals(CardAddressValueObject other)
+        {
+            return Value == other.Value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CardAddressValueObject other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return Value != null ? Value.GetHashCode() : 0;
+        }
 
         private readonly string _value;
     }
