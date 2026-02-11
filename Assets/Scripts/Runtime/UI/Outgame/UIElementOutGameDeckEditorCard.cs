626ee820680f33067b5ffe241256326eae74f3d5
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-11 18:15:24 +0900

    [update]アイコンと説明文をバインドできるように

--- Assets/Scripts/Runtime/UI/Outgame/UIElementOutGameDeckEditorCard.cs
+++ Assets/Scripts/Runtime/UI/Outgame/UIElementOutGameDeckEditorCard.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Cryptos.Runtime.UI.Outgame.Deck
@@ -6,14 +7,25 @@ namespace Cryptos.Runtime.UI.Outgame.Deck
     [UxmlElement]
     public partial class UIElementOutGameDeckEditorCard : VisualElementBase
     {
-        public UIElementOutGameDeckEditorCard() : base("DeckEditorCard") { }
+        public UIElementOutGameDeckEditorCard() : base("DeckEditorCard", InitializeType.Absolute) { }
+
+        public void BindCardData(Texture2D texture, string explanation)
+        {
+            _icon.style.backgroundImage = texture;
+            _explanation.text = explanation;
+        }
 
         protected override ValueTask Initialize_S(VisualElement root)
         {
+            _icon = root.Q<VisualElement>(ICON_NAME);
+            _explanation = root.Q<Label>(EXPLANATION_NAME);
             return default;
         }
 
+        private const string EXPLANATION_NAME = "explanation";
+        private const string ICON_NAME = "icon";
 
-
+        private VisualElement _icon;
+        private Label _explanation;
     }
 }
