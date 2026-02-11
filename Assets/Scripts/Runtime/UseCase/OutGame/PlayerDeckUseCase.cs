262f6fcfbc5ef63c0ac392b91a76be3def7523dd
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-11 17:02:41 +0900

    [update]デッキ名のValueObjectに対応

--- Assets/Scripts/Runtime/UseCase/OutGame/PlayerDeckUseCase.cs
+++ Assets/Scripts/Runtime/UseCase/OutGame/PlayerDeckUseCase.cs
@@ -1,6 +1,10 @@
 using Cryptos.Runtime.Entity;
+using Cryptos.Runtime.Entity.Outgame.Card;
 using Cryptos.Runtime.Entity.System.SaveData;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Cryptos.Runtime.Entity.Ingame.Card; // DeckCardEntityのために追加
 
 namespace Cryptos.Runtime.UseCase.OutGame
 {
@@ -10,14 +14,17 @@ namespace Cryptos.Runtime.UseCase.OutGame
     public class PlayerDeckUseCase
     {
         private readonly PlayerDeckSaveData _playerDeckSaveData;
+        private readonly ICardRepository _cardRepository; // 追加
 
         /// <summary>
         ///     PlayerDeckUseCaseの新しいインスタンスを生成する。
         /// </summary>
         /// <param name="playerDeckSaveData">プレイヤーのデッキ保存データ。</param>
-        public PlayerDeckUseCase(PlayerDeckSaveData playerDeckSaveData)
+        /// <param name="cardRepository">カードリポジトリ。</param> // 追加
+        public PlayerDeckUseCase(PlayerDeckSaveData playerDeckSaveData, ICardRepository cardRepository) // 引数追加
         {
             _playerDeckSaveData = playerDeckSaveData;
+            _cardRepository = cardRepository; // 追加
         }
 
         /// <summary>
@@ -25,7 +32,7 @@ namespace Cryptos.Runtime.UseCase.OutGame
         /// </summary>
         /// <param name="deckName">取得するデッキの名前。</param>
         /// <returns>指定された名前のカードアドレスの配列。存在しない場合はnullを返す。</returns>
-        public CardAddressValueObject[] GetDeck(string deckName)
+        public CardAddressValueObject[] GetDeck(DeckNameValueObject deckName)
         {
             return _playerDeckSaveData.GetDeck(deckName);
         }
@@ -34,7 +41,7 @@ namespace Cryptos.Runtime.UseCase.OutGame
         ///     保存されているすべてのデッキの名前を取得する。
         /// </summary>
         /// <returns>保存されているデッキの名前のコレクション。</returns>
-        public IReadOnlyCollection<string> GetAllDeckNames()
+        public IReadOnlyCollection<DeckNameValueObject> GetAllDeckNames()
         {
             return _playerDeckSaveData.GetAllDeckNames();
         }
@@ -44,11 +51,28 @@ namespace Cryptos.Runtime.UseCase.OutGame
         /// </summary>
         /// <param name="deckName">登録するデッキの名前。</param>
         /// <param name="deck">登録するカードアドレスの配列。</param>
-        public void RegisterDeck(string deckName, CardAddressValueObject[] deck)
+        public void RegisterDeck(DeckNameValueObject deckName, CardAddressValueObject[] deck)
         {
             _playerDeckSaveData.RegisterDeck(deckName, deck);
             SaveExecuter.DeckSave(); // 保存処理を呼び出す
         }
 
+        /// <summary>
+        ///     カードアドレスからDeckCardEntityを生成する。
+        /// </summary>
+        /// <param name="cardAddress">カードアドレス。</param>
+        /// <returns>生成されたDeckCardEntity。</returns>
+        public async Task<DeckCardEntity> CreateDeckCardEntity(CardAddressValueObject cardAddress)
+        {
+            var deckCardEntity = await _cardRepository.GetDeckCardEntityAsync(cardAddress); // GetDeckCardEntityAsync を呼び出す
+
+            if (deckCardEntity == null)
+            {
+                Debug.LogError($"DeckCardEntity not found or invalid for address: {cardAddress.Value}");
+                return null;
+            }
+
+            return deckCardEntity;
+        }
     }
 }
