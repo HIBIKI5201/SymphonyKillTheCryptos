e83e9421a83d0e9e289f8385a549e806a268000c
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-11 21:48:32 +0900

    [update]エディットボタンを追加

--- Assets/Scripts/Runtime/UI/Outgame/OutgameUIManager.cs
+++ Assets/Scripts/Runtime/UI/Outgame/OutgameUIManager.cs
@@ -24,6 +24,8 @@ namespace Cryptos.Runtime.UI.Outgame
 
         protected override async Task InitializeDocumentAsync(UIDocument document, VisualElement root)
         {
+            _deckEditor = root.Q<UIElementDeckEditor>();
+
             VisualElement buttonContainer = root.Q<VisualElement>(BUTTONS_NAME);
            if (buttonContainer == null) { return; }
 
@@ -31,9 +33,10 @@ namespace Cryptos.Runtime.UI.Outgame
             if (_startButton == null) { return; }
 
             _startButton.RegisterCallback<NavigationSubmitEvent>(HandleStartButtonPressed);
-            _startButton.Focus(); // 初期状態はスタートボタンをフォーカスする。
-
-            _deckEditor = root.Q<UIElementDeckEditor>();
+            _startButton.Focus();
+            
+            _editButton = buttonContainer.Q<Button>(EDIT_BUTTON_NAME);
+            _editButton.clicked += _deckEditor.Show;
 
             destroyCancellationToken.Register(() =>
             {
@@ -48,8 +51,10 @@ namespace Cryptos.Runtime.UI.Outgame
 
         private const string BUTTONS_NAME = "buttons";
         private const string START_BUTTON_NAME = "start";
+        private const string EDIT_BUTTON_NAME = "edit";
 
         private Button _startButton;
+        private Button _editButton;
 
         private event Action _onStartButtonPressed;
         private UIElementDeckEditor _deckEditor;
