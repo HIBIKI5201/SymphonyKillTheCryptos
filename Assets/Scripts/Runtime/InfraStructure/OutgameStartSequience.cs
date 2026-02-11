2937ed2d0ddcfc056407204eb1a274f8915b1773
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-11 18:16:09 +0900

    [update]シークエンスを更新

--- Assets/Scripts/Runtime/InfraStructure/OutgameStartSequience.cs
+++ Assets/Scripts/Runtime/InfraStructure/OutgameStartSequience.cs
@@ -1,20 +1,40 @@
+using Cryptos.Runtime.Entity.System.SaveData;
 using Cryptos.Runtime.Presenter;
 using Cryptos.Runtime.Presenter.Ingame.System;
+using Cryptos.Runtime.Presenter.OutGame;
 using Cryptos.Runtime.UI.Outgame;
+using Cryptos.Runtime.UseCase.OutGame;
 using SymphonyFrameWork.System;
 using System.Threading.Tasks;
+using UnityEngine; // For Debug.Log (temporary)
 
 namespace Cryptos.Runtime.InfraStructure
 {
+    /// <summary>
+    ///     アウトゲームの初期化シーケンスを管理するクラスである。
+    /// </summary>
     public class OutgameStartSequience : IGameInstaller
     {
         public async ValueTask GameInitialize()
         {
+            // サービスロケーターから必要なインスタンスを取得。
             OutgameUIManager uiManager = await ServiceLocator.GetInstanceAsync<OutgameUIManager>();
             OutgameManager outgameManager = await ServiceLocator.GetInstanceAsync<OutgameManager>();
-
             await InitializeUtility.WaitInitialize(uiManager);
 
+            // プレイヤーデッキデータをロード。
+            PlayerDeckSaveData playerDeckSaveData = SaveDataSystem<PlayerDeckSaveData>.Data;
+            PlayerMasterSaveData playerMasterData = SaveDataSystem<PlayerMasterSaveData>.Data;
+
+            CardRepositoryImpl cardRepositoryImpl = new CardRepositoryImpl();
+            PlayerMasterUseCase playerMasterUseCase = new(playerMasterData);
+            PlayerDeckUseCase playerDeckUseCase = new(playerDeckSaveData, cardRepositoryImpl);
+            DeckEditorPresenter deckEditorPresenter = new(playerDeckUseCase, playerMasterUseCase);
+
+            IDeckEditorUI deckEditorUI = uiManager.DeckEditor;
+            deckEditorUI.SetPresenter(deckEditorPresenter);
+
+            // スタートボタンが押された際のイベントハンドラを設定
             uiManager.OnPressedStartButton += HandlePressedStartButton;
 
             void HandlePressedStartButton()
