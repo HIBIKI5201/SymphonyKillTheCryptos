73d7da28d9a3fd4105ac6eb0c2d9e8ef0a21db8c
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-11 21:25:43 +0900

    [update]カードデータをViewModelに変換して表示

--- Assets/Scripts/Runtime/Presenter/Outgame/DeckEditorPresenter.cs
+++ Assets/Scripts/Runtime/Presenter/Outgame/DeckEditorPresenter.cs
@@ -5,9 +5,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UnityEngine; // 追加
 
 namespace Cryptos.Runtime.Presenter.OutGame
 {
+    /// <summary>
+    ///     カード表示用のViewModel。UIはこれを通してカード情報を参照する。
+    /// </summary>
+    public readonly struct CardViewModel
+    {
+        public readonly Texture2D CardIcon;
+        public readonly string CardExplanation;
+        public readonly CardAddressValueObject Address; // Add Address property
+        public string AddressStr => Address.Value;
+
+        // Remove constructor with CardData entity
+        // public CardViewModel(CardData entity)
+        // {
+        //     CardIcon = entity.CardIcon;
+        //     CardExplanation = entity.CardExplanation;
+        // }
+
+        public CardViewModel(DeckCardEntity entity) // New constructor
+        {
+            Address = entity.Address;
+            CardIcon = entity.CardIcon;
+            CardExplanation = entity.CardExplanation;
+        }
+
+        public CardViewModel(CardAddressValueObject address, Texture2D cardIcon, string cardExplanation) // Modified constructor
+        {
+            Address = address;
+            CardIcon = cardIcon;
+            CardExplanation = cardExplanation;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CardViewModel other)) return false;
+            return Address.Equals(other.Address);
+        }
+
+        public override int GetHashCode()
+        {
+            return Address.GetHashCode();
+        }
+    }
+
     /// <summary>
     ///     デッキ編集画面のUIとユースケースを繋ぐプレゼンターである。
     /// </summary>
@@ -16,6 +60,14 @@ namespace Cryptos.Runtime.Presenter.OutGame
         private readonly PlayerDeckUseCase _playerDeckUseCase;
         private readonly PlayerMasterUseCase _playerMasterUseCase;
         private IDeckEditorUI _deckEditorUI;
+
+        private List<string> _roles = new List<string> { "Warrior", "Mage", "Healer" }; // 仮のロールリスト
+        private int _currentRoleIndex = 0;
+
+        private List<CardViewModel> _currentDeckCards = new List<CardViewModel>(); // 現在のデッキのスロットごとのカード
+        private List<CardViewModel> _ownedCards = new List<CardViewModel>(); // 所持している全カード
+        private CardViewModel? _selectedCardForSwap; // 交換のために選択されたカード
+        private int _currentDeckCardIndex = 0; // 現在デッキ内で選択されているカードのインデックス
 
         /// <summary>
         ///     DeckEditorPresenterの新しいインスタンスを生成する。
@@ -28,89 +80,144 @@ namespace Cryptos.Runtime.Presenter.OutGame
             _playerMasterUseCase = playerMasterUseCase;
         }
 
-        /// <summary>
-        ///     UIを登録する。
-        /// </summary>
-        /// <param name="ui">デッキエディタUIのインターフェース。</param>
         public void RegisterUI(IDeckEditorUI ui)
         {
             _deckEditorUI = ui;
+
+            // UIからのイベントを購読
+            _deckEditorUI.OnEditButtonClicked += OnEdit;
+            _deckEditorUI.OnSaveButtonClicked += OnSave;
+            _deckEditorUI.OnRoleSelected += OnRoleChange;
+            _deckEditorUI.OnCancelButtonClicked += OnCancel;
+            _deckEditorUI.OnOwnedCardSelected += OnOwnedCardSelected;
+            _deckEditorUI.OnCardSwapRequested += OnRequestCardSwap;
+            _deckEditorUI.OnNavigateDeckCard += OnNavigateDeckCard; // イベントを購読
         }
 
         /// <summary>
-        ///     すべてのデッキ名をUIに表示する。
+        ///     デッキエディタの初期化を行う。
         /// </summary>
-        public async Task DisplayAllDeckNames()
+        public void InitializeDeckEditor()
         {
-            IReadOnlyCollection<DeckNameValueObject> deckNames = _playerDeckUseCase.GetAllDeckNames();
-            _deckEditorUI.ShowDeckNames(deckNames.Select(dn => dn.Value).ToList());
+            // ロール情報や初期デッキ情報などをUIに渡すロジック
+            _deckEditorUI.SetStatusText($"現在のロール: {_roles[_currentRoleIndex]}");
+            _deckEditorUI.SetRoleCharacter(_roles[_currentRoleIndex]);
 
-            // 現在選択されているデッキがあれば、それをUIに反映
-            DeckNameValueObject selectedDeckNameVO = _playerMasterUseCase.GetSelectedDeckName();
-            if (!string.IsNullOrEmpty(selectedDeckNameVO.Value) && deckNames.Any(dn => dn.Value == selectedDeckNameVO.Value))
+            // ダミーカードデータ
+            var dummyCard1 = new CardViewModel(new CardAddressValueObject("card001"), Texture2D.redTexture, "Description for Card A");
+            var dummyCard2 = new CardViewModel(new CardAddressValueObject("card002"), Texture2D.blackTexture, "Description for Card B");
+            var dummyCard3 = new CardViewModel(new CardAddressValueObject("card003"), Texture2D.grayTexture, "Description for Card C");
+            var dummyCard4 = new CardViewModel(new CardAddressValueObject("card004"), Texture2D.whiteTexture, "Description for Card D");
+
+            _currentDeckCards = new List<CardViewModel> { dummyCard1, dummyCard2, dummyCard3 }; // デッキ内のカード
+            _ownedCards = new List<CardViewModel> { dummyCard1, dummyCard2, dummyCard3, dummyCard4 }; // 所持カード
+
+            // UIを更新
+            _deckEditorUI.SetCurrentCard(_currentDeckCards[0]); // 仮でデッキの最初のカードを中央に表示
+            _deckEditorUI.SetAdjacentCards(dummyCard3, _currentDeckCards[1]); // 仮で左右のカードを設定
+            _deckEditorUI.SetOwnedCards(_ownedCards);
+            UpdateDeckCardDisplay(); // 初期表示を更新
+        }
+
+        private void OnEdit()
+        {
+            UnityEngine.Debug.Log("Presenter: Edit button clicked!");
+            // TODO: 編集モードへの遷移ロジック
+        }
+
+        private event Action OnDeckSavedAndClosed; // デッキ保存後、画面を閉じるためのイベント
+
+        private void OnSave()
+        {
+            // デッキを保存
+            DeckNameValueObject deckName = new("DefaultDeck"); // 仮のデッキ名
+            CardAddressValueObject[] deckAddresses = _currentDeckCards.Select(card => card.Address).ToArray();
+            _playerDeckUseCase.RegisterDeck(deckName, deckAddresses);
+
+            // 保存成功メッセージをUIに表示
+            // _deckEditorUI.ShowSuccessMessage($"デッキ '{deckName.Value}' を保存しました。");
+
+            OnDeckSavedAndClosed?.Invoke(); // デッキ保存後、画面を閉じるイベントを発火
+            UnityEngine.Debug.Log("Presenter: Deck saved!");
+        }
+
+        private void OnRoleChange()
+        {
+            _currentRoleIndex = (_currentRoleIndex + 1) % _roles.Count;
+            string newRole = _roles[_currentRoleIndex];
+            _deckEditorUI.SetStatusText($"現在のロール: {newRole}");
+            _deckEditorUI.SetRoleCharacter(newRole);
+            UnityEngine.Debug.Log($"Presenter: Role changed to {newRole}!");
+            // TODO: ロール変更に伴うデッキ制限やUIの更新ロジック
+        }
+
+        private void OnCancel()
+        {
+            UnityEngine.Debug.Log("Presenter: Cancel button clicked!");
+            // TODO: キャンセル処理ロジック
+        }
+
+        private void OnOwnedCardSelected(CardViewModel card)
+        {
+            _selectedCardForSwap = card;
+            _deckEditorUI.SetSelectedOwnedCard(card);
+            UnityEngine.Debug.Log($"Presenter: Owned card selected for swap: {card.CardExplanation}");
+        }
+
+        private void OnRequestCardSwap()
+        {
+            if (_selectedCardForSwap == null || _currentDeckCards.Count == 0)
             {
-                _deckEditorUI.SetSelectedDeckInDropdown(selectedDeckNameVO.Value);
-                await LoadAndDisplayDeck(selectedDeckNameVO.Value);
+                UnityEngine.Debug.LogWarning("Presenter: Cannot swap cards. No card selected for swap or deck is empty.");
+                return;
             }
+
+            // 現在デッキで選択されているカード
+            CardViewModel targetDeckCard = _currentDeckCards[_currentDeckCardIndex];
+
+            // _ownedCards から _selectedCardForSwap を削除し、targetDeckCard を追加
+            _ownedCards.Remove(_selectedCardForSwap.Value);
+            _ownedCards.Add(targetDeckCard);
+
+            // _currentDeckCards の該当位置を _selectedCardForSwap で置き換える
+            _currentDeckCards[_currentDeckCardIndex] = _selectedCardForSwap.Value;
+
+            // UIを更新
+            UpdateDeckCardDisplay();
+            _deckEditorUI.SetOwnedCards(_ownedCards);
+            _deckEditorUI.ClearSelectedOwnedCard(); // 選択状態をクリア
+
+            _selectedCardForSwap = null; // 交換完了後、選択カードをクリア
+
+            UnityEngine.Debug.Log($"Presenter: Cards swapped! Deck card {_currentDeckCardIndex} replaced with {_selectedCardForSwap.Value.CardExplanation}");
         }
 
-        /// <summary>
-        ///     指定された名前のデッキをロードし、UIに表示する。
-        /// </summary>
-        /// <param name="deckName">ロードするデッキの名前。</param>
-        public async Task LoadAndDisplayDeck(string name)
+        private void OnNavigateDeckCard(UnityEngine.UIElements.NavigationMoveEvent.Direction direction)
         {
-            DeckNameValueObject deckName = new(name);
+            if (_currentDeckCards.Count == 0) return;
 
-            CardAddressValueObject[] deck = _playerDeckUseCase.GetDeck(deckName);
-            if (deck != null)
+            if (direction == UnityEngine.UIElements.NavigationMoveEvent.Direction.Left)
             {
-                // DeckViewModelに変換してUIに渡す。
-                var deckCardEntityTasks = deck.Select(async cardAddress => await _playerDeckUseCase.CreateDeckCardEntity(cardAddress));
-                DeckCardEntity[] deckCardEntities = await Task.WhenAll(deckCardEntityTasks);
-
-                DeckViewModel deckViewModel = new(deckName, deckCardEntities);
-                _deckEditorUI.ShowDeck(deckViewModel);
-                _playerMasterUseCase.SetSelectedDeckName(deckName);
+                _currentDeckCardIndex = (_currentDeckCardIndex - 1 + _currentDeckCards.Count) % _currentDeckCards.Count;
             }
-            else
+            else if (direction == UnityEngine.UIElements.NavigationMoveEvent.Direction.Right)
             {
-                _deckEditorUI.ShowErrorMessage($"デッキ '{deckName.Value}' が見つかりませんでした。");
+                _currentDeckCardIndex = (_currentDeckCardIndex + 1) % _currentDeckCards.Count;
             }
+
+            UpdateDeckCardDisplay();
         }
 
-        /// <summary>
-        ///     新しいデッキを作成し、UIに表示する。
-        /// </summary>
-        /// <param name="deckNameVO">新しいデッキの名前。</param>
-        public async void CreateNewDeck(string name)
+        private void UpdateDeckCardDisplay()
         {
-            DeckNameValueObject deckNameVO = new(name);
+            if (_currentDeckCards.Count == 0) return;
 
-            CardAddressValueObject[] emptyDeck = Array.Empty<CardAddressValueObject>();
-            _playerDeckUseCase.RegisterDeck(deckNameVO, emptyDeck);
+            var currentCard = _currentDeckCards[_currentDeckCardIndex];
+            var leftCard = _currentDeckCards[(_currentDeckCardIndex - 1 + _currentDeckCards.Count) % _currentDeckCards.Count];
+            var rightCard = _currentDeckCards[(_currentDeckCardIndex + 1) % _currentDeckCards.Count];
 
-            var deckCardEntityTasks = emptyDeck.Select(async cardAddress => await _playerDeckUseCase.CreateDeckCardEntity(cardAddress));
-            DeckCardEntity[] deckCardEntities = await Task.WhenAll(deckCardEntityTasks);
-
-            DeckViewModel deckViewModel = new(deckNameVO, deckCardEntities);
-            _deckEditorUI.ShowDeck(deckViewModel);
-            _deckEditorUI.ShowSuccessMessage($"デッキ '{deckNameVO.Value}' を作成しました。");
-            _playerMasterUseCase.SetSelectedDeckName(deckNameVO);
-            await DisplayAllDeckNames();
-        }
-
-        /// <summary>
-        ///     現在のデッキを保存する。
-        /// </summary>
-        /// <param name="deckName">保存するデッキの名前。</param>
-        /// <param name="deck">保存するカードアドレスの配列。</param>
-        public async void SaveDeck(DeckNameValueObject deckName, CardAddressValueObject[] deck) // async に変更
-        {
-            _playerDeckUseCase.RegisterDeck(deckName, deck);
-            _playerMasterUseCase.SetSelectedDeckName(deckName);
-            _deckEditorUI.ShowSuccessMessage($"デッキ '{deckName.Value}' を保存しました。");
-            await DisplayAllDeckNames();
+            _deckEditorUI.SetCurrentCard(currentCard);
+            _deckEditorUI.SetAdjacentCards(leftCard, rightCard);
         }
     }
 }
