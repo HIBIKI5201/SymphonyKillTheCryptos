4f96230eb51398bc430111c605bd5a121888bfe7
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-13 16:42:04 +0900

    [update]Jsonで保存できる形式に変更

--- Assets/Scripts/Runtime/Entity/System/PlayerDeckSaveData.cs
+++ Assets/Scripts/Runtime/Entity/System/PlayerDeckSaveData.cs
@@ -2,6 +2,7 @@ using Cryptos.Runtime.Entity.Outgame.Card;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using static UnityEditor.LightingExplorerTableColumn;
 
@@ -44,17 +45,21 @@ namespace Cryptos.Runtime.Entity.System.SaveData
 
         public static void Save()
         {
+            // シリアライズできる形に整形する。
+            Data._deckData = Data._deck.Select(
+                kv => new SaveDataStruct(kv.Key.Value, kv.Value.Select(a => a.Value).ToArray())).ToArray();
+
             string text = JsonConvert.SerializeObject(Data);
-            PlayerPrefs.SetString(typeof(DataType).Name, text);
+            PlayerPrefs.SetString(typeof(PlayerDeckSaveData).Name, text);
             Debug.Log("[SaveDataSystem]\nデータをセーブしました\n" + text);
         }
 
         public static void Load()
         {
-            string value = PlayerPrefs.GetString(typeof(DataType).Name);
+            string value = PlayerPrefs.GetString(typeof(PlayerDeckSaveData).Name);
             if (string.IsNullOrEmpty(value))
             {
-                Debug.Log("[SaveDataSystem]\n" + typeof(DataType).Name + "のデータが見つからないので生成しました");
+                Debug.Log("[SaveDataSystem]\n" + typeof(PlayerDeckSaveData).Name + "のデータが見つからないので生成しました");
                 Data = new PlayerDeckSaveData();
                 return;
             }
@@ -62,12 +67,15 @@ namespace Cryptos.Runtime.Entity.System.SaveData
             PlayerDeckSaveData saveData = JsonConvert.DeserializeObject<PlayerDeckSaveData>(value);
             if (saveData != null)
             {
-                Debug.Log(string.Format("[{0}]\n{1}のデータがロードされました\n{2}", "SaveDataSystem", typeof(DataType).Name, saveData));
+                Debug.Log(string.Format("[{0}]\n{1}のデータがロードされました\n{2}", "SaveDataSystem", typeof(PlayerDeckSaveData).Name, saveData));
                 Data = saveData;
+                Data._deck = Data._deckData
+                    .ToDictionary(kv => new DeckNameValueObject(kv.Name),
+                    kv => kv.Value.Select(v => new CardAddressValueObject(v)).ToArray());
             }
             else
             {
-                Debug.LogWarning("[SaveDataSystem]\n" + typeof(DataType).Name + "のロードが出来ませんでした\n新たなインスタンスを生成します");
+                Debug.LogWarning("[SaveDataSystem]\n" + typeof(PlayerDeckSaveData).Name + "のロードが出来ませんでした\n新たなインスタンスを生成します");
                 Data = new PlayerDeckSaveData();
             }
         }
@@ -75,5 +83,21 @@ namespace Cryptos.Runtime.Entity.System.SaveData
         public bool IsExist(DeckNameValueObject deck) => _deck.ContainsKey(deck);
 
         private Dictionary<DeckNameValueObject, CardAddressValueObject[]> _deck = new();
+
+        [JsonProperty]
+        private SaveDataStruct[] _deckData = Array.Empty<SaveDataStruct>();
+
+        [Serializable]
+        private struct SaveDataStruct
+        {
+            public SaveDataStruct(string name, string[] value)
+            {
+                Name = name;
+                Value = value;
+            }
+
+            public string Name; 
+            public string[] Value;
+        }
     }
 }
