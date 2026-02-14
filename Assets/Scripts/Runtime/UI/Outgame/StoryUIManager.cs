17b1c993d49d149a0d21b55bd6a735a1646a1061
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-14 22:39:05 +0900

    [add]ストーリー用のUIマネージャーを追加

---
+++ Assets/Scripts/Runtime/UI/Outgame/StoryUIManager.cs
@@ -0,0 +1,15 @@
+using Cryptos.Runtime.UI.Basis;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Cryptos.Runtime.UI
+{
+    public class StoryUIManager : UIManagerBase
+    {
+        protected override ValueTask InitializeDocumentAsync(UIDocument document, VisualElement root)
+        {
+            return default;
+        }
+    }
+}
