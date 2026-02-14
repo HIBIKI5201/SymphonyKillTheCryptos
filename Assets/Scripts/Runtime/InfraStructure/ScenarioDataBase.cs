a09ed4ced77ed8cb5966d01cd06afcf6e40f0631
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-15 02:00:19 +0900

    [update]データベースはデータアセットのリポジトリに

--- Assets/Scripts/Runtime/InfraStructure/ScenarioDataBase.cs
+++ Assets/Scripts/Runtime/InfraStructure/ScenarioDataBase.cs
@@ -8,12 +8,13 @@ namespace Cryptos.Runtime.InfraStructure.OutGame.Story
         menuName = CryptosPathConstant.ASSET_PATH + nameof(ScenarioDataBase))]
     public class ScenarioDataBase : ScriptableObject
     {
-        public ScenarioData GetDataBase()
+        public ScenarioData GetScenarioData(int index)
         {
-            return new(_scenarioNodes);
+            ScenarioData data = _assets[index].GetDataBase();
+            return data;
         }
 
         [SerializeField]
-        private ScenarioNode[] _scenarioNodes;
+        private ScenarioDataAsset[] _assets;
     }
 }
