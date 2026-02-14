cb079b668d170582b6580e18095d53b0d0e8aba4
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-15 02:00:36 +0900

    [update]サービスロケーターからインスタンスを取得する

--- Assets/Scripts/Runtime/InfraStructure/StoryStartSequence.cs
+++ Assets/Scripts/Runtime/InfraStructure/StoryStartSequence.cs
@@ -2,6 +2,7 @@ using Cryptos.Runtime.Entity.Outgame.Story;
 using Cryptos.Runtime.Presenter.Ingame.System;
 using Cryptos.Runtime.Presenter.OutGame.Story;
 using Cryptos.Runtime.UseCase.OutGame.Story;
+using SymphonyFrameWork.System;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -9,19 +10,26 @@ namespace Cryptos.Runtime.InfraStructure.OutGame.Story
 {
     public class StoryStartSequence : IGameInstaller
     {
-        public ValueTask GameInitialize()
+        public async ValueTask GameInitialize()
         {
             NovelSettingEntity setting = _settingAsset.Create();
+            ScenarioDataEntity data = await ServiceLocator.GetInstanceAsync<ScenarioDataEntity>();
+
+            ScenarioData scenario = _scenarioDataBase.GetScenarioData(data.PlayIndex);
+
             ScenarioActionRepository actionRepository =
                 new ScenarioActionRepository(
                 _cameraRep);
+            ScenarioPlayer player = new(
+                scenario);
 
-            return default;
         }
 
         [SerializeField]
         private NovelSettingAsset _settingAsset;
         [SerializeField]
         private StoryCameraRepository _cameraRep;
+        [SerializeField]
+        private ScenarioDataBase _scenarioDataBase;
     }
 }
