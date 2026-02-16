20d49028e99bfde6f2498266d9124db902a89cca
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-16 16:57:34 +0900

    [update]メッセージウィンドウを取得する

--- Assets/Scripts/Runtime/UI/Outgame/StoryUIManager.cs
+++ Assets/Scripts/Runtime/UI/Outgame/StoryUIManager.cs
@@ -1,14 +1,44 @@
 using Cryptos.Runtime.UI.Basis;
+using System;
 using System.Threading.Tasks;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
 using UnityEngine.UIElements;
 
 namespace Cryptos.Runtime.UI.Outgame.Story
 {
     public class StoryUIManager : UIManagerBase
     {
-        protected override ValueTask InitializeDocumentAsync(UIDocument document, VisualElement root)
+        public event Action OnNextClicked;
+        public StoryMessageWindow MessageWindow => _messageWindow;
+
+        protected override async ValueTask InitializeDocumentAsync(UIDocument document, VisualElement root)
         {
-            return default;
+            _messageWindow = root.Q<StoryMessageWindow>();
+
+            if (EventSystem.current.TryGetComponent(out InputSystemUIInputModule module))
+            {
+                _uiModule = module;
+                module.submit.action.started += NextClicked;
+            }
+
+            await _messageWindow.InitializeTask;
+        }
+
+        private InputSystemUIInputModule _uiModule;
+        private StoryMessageWindow _messageWindow;
+
+        private void OnDisable()
+        {
+            if (_uiModule == null) { return; }
+
+            _uiModule.submit.action.started -= NextClicked;
+        }
+
+        private void NextClicked(InputAction.CallbackContext context)
+        {
+            OnNextClicked?.Invoke();
         }
     }
 }
