aa5e1c4272f8fa4db3363cc8fa998c25631a054c
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-17 15:56:42 +0900

    [fix]ツールの定数の間違いを修正

--- Assets/Scripts/Runtime/Utility/Constant/CryptosPathConstant.cs
+++ Assets/Scripts/Runtime/Utility/Constant/CryptosPathConstant.cs
@@ -2,7 +2,7 @@ namespace Cryptos.Runtime.Framework
 {
     public static class CryptosPathConstant
     {
-        public const string TOOL_MENU_ITEM_PATH = "Tool/Cryptos/";
+        public const string TOOL_MENU_ITEM_PATH = "Tools/Cryptos/";
         public const string ASSET_PATH = "Cryptos/";
     }
 }
