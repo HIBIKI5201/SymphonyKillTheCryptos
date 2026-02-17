e7bcc569fe6213dc61219718eb55f228900502e7
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-17 11:09:54 +0900

    [add]リポジトリ実装にキャラを追加

--- Assets/Scripts/Runtime/UseCase/OutGame/Story/ScenarioAction/ScenarioActionRepository.cs
+++ Assets/Scripts/Runtime/UseCase/OutGame/Story/ScenarioAction/ScenarioActionRepository.cs
@@ -1,3 +1,4 @@
+using Cryptos.Runtime.Entity;
 using Cryptos.Runtime.Entity.Outgame.Story;
 
 namespace Cryptos.Runtime.UseCase.OutGame.Story
@@ -5,13 +6,17 @@ namespace Cryptos.Runtime.UseCase.OutGame.Story
     public class ScenarioActionRepository : IScenarioActionRepository
     {
         public ScenarioActionRepository(
-            ICameraRepository cameraRep)
+            ICameraRepository cameraRep,
+            ICharacterRepository charaRep)
         {
             _cameraRep = cameraRep;
+            _charaRep = charaRep;
         }
 
         public ICameraRepository CameraRepository => _cameraRep;
+        public ICharacterRepository CharacterRepository => _charaRep;
 
         private readonly ICameraRepository _cameraRep;
+        private readonly ICharacterRepository _charaRep;
     }
 }
