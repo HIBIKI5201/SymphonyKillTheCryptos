4e1c357e28067618a7211162f99c234f97585bda
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-17 11:20:20 +0900

    [add]キャラクターを出現させる

---
+++ Assets/Scripts/Runtime/UseCase/OutGame/Story/ScenarioAction/CharacterShow.cs
@@ -0,0 +1,27 @@
+using Cryptos.Runtime.Entity.Outgame.Story;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Cryptos.Runtime.UseCase
+{
+    public class CharacterShow : CharacterActionBase
+    {
+        public CharacterShow() : base(string.Empty) { }
+
+        public CharacterShow(string name, float x, float y) : base(name)
+        {
+            _position = new(x, y);
+        }
+
+        public override ValueTask ExecuteAsync(IScenarioActionRepository repository, IPauseSubject pauseHandler, CancellationToken token = default)
+        {
+            repository.CharacterRepository.Show(_name);
+            repository.CharacterRepository.Move(_name, _position, 0f);
+
+            return default;
+        }
+
+        private Vector2 _position;
+    }
+}
