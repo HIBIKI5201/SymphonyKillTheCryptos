5b1489ca2aa42f8fc4f62b432f21683b6c50a1bf
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-17 11:20:52 +0900

    [update]範囲外の例外ログを追加

--- Assets/Scripts/Runtime/InfraStructure/StoryCameraRepository.cs
+++ Assets/Scripts/Runtime/InfraStructure/StoryCameraRepository.cs
@@ -1,8 +1,9 @@
-using Cryptos.Runtime.UseCase;
+using Cryptos.Runtime.Entity.Outgame.Story;
+using System.Linq;
 using Unity.Cinemachine;
 using UnityEngine;
 
-namespace Cryptos.Runtime.Presenter.OutGame.Story
+namespace Cryptos.Runtime.InfraStructure.OutGame.Story
 {
     public class StoryCameraRepository : MonoBehaviour, ICameraRepository
     {
@@ -10,7 +11,25 @@ namespace Cryptos.Runtime.Presenter.OutGame.Story
 
         public void SetActiveCamera(int index)
         {
+            if (0 <= index && index < _cameras.Length)  
+            {
+                Debug.LogError($"{index} はCameraRepositoryの範囲外です。");
+                return; 
+            }
+
             CinemachineCamera camera = _cameras[index];
+            camera.Prioritize();
+        }
+
+        public void SetActiveCamera(string name)
+        {
+            CinemachineCamera camera = _cameras.First(c => c.Name == name);
+            if (camera == null) 
+            {
+                Debug.LogError($"{name} はCameraRepositoryに存在しません。");
+                return;
+            }
+
             camera.Prioritize();
         }
 
