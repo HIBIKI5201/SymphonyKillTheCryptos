8b1ef01e99829c64db2209551abf0250478515a1
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-17 11:09:12 +0900

    [add]キャラのリポジトリを追加

---
+++ Assets/Scripts/Runtime/InfraStructure/StoryCharacterRepository.cs
@@ -0,0 +1,62 @@
+using Cryptos.Runtime.Entity.Outgame.Story;
+using SymphonyFrameWork.Utility;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Cryptos.Runtime.InfraStructure
+{
+    public class StoryCharacterRepository : MonoBehaviour, ICharacterRepository
+    {
+        public void Show(string name)
+        {
+            if (_showCharacters.TryGetValue(name, out GameObject c))
+            {
+                c.SetActive(true);
+                return;
+            }
+
+            GameObject target = null;
+            for (int i = 0; i < _characters.Length; i++)
+            {
+                GameObject chara = _characters[i];
+                if (chara.name == name)
+                {
+                    target = chara;
+                    break;
+                }
+            }
+
+            target = Instantiate(target);
+            _showCharacters.Add(name, target);
+        }
+
+
+        public void Hide(string name)
+        {
+            if (!_showCharacters.TryGetValue(name, out GameObject c)) { return; }
+            c.SetActive(false);
+        }
+
+        public async void Move(string name, Vector2 position, float duration)
+        {
+            if (_showCharacters.TryGetValue(name, out GameObject character))
+            {
+                await MoveAsync(character.transform, position, duration);
+            }
+        }
+
+        private GameObject[] _characters;
+
+        private Dictionary<string, GameObject> _showCharacters = new();
+
+        private async ValueTask MoveAsync(Transform target, Vector2 pos, float d)
+        {
+            if (d <= 0) { target.position = pos; }
+
+            await SymphonyTween.Tweening<Vector2>(target.position,
+                p => target.position = p,
+                pos, d);
+        }
+    }
+}
